caa16e21cbae75c54a1ea3f04756996704f283fc
jakeogh <github.com@v6y.net>
2017-02-13 23:39:08 -0700

    auto-commit

--- kcl/iterops.py
+++ kcl/iterops.py
@@ -2,6 +2,7 @@
 
 from itertools import zip_longest
 
+# https://docs.python.org/3/library/itertools.html#itertools-recipes
 def grouper(iterable, n, fillvalue=None):
     args = [iter(iterable)] * n
     return zip_longest(*args, fillvalue=fillvalue)
