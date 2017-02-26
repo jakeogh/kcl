f076e054eaf9e3003420328988c713c449627b5d
jakeogh <github.com@v6y.net>
2017-02-25 22:26:57 -0700

    auto-commit

--- kcl/timeops.py
+++ kcl/timeops.py
@@ -9,3 +9,14 @@ def timestamp():
 if __name__ == '__main__':
     timestamp = timestamp()
     print(timestamp)
+
+
+def timeit(f):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = f(*args, **kw)
+        te = time.time()
+        print('func:%r args:[%r, %r] took: %2.4f sec' % (f.__name__, args, kw, te-ts))
+        return result
+    return timed
+
