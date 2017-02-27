b9c41154142053b0036781efcd6bbc9d3ad4ea1b
jakeogh <github.com@v6y.net>
2017-02-27 04:26:09 -0700

    auto-commit

--- kcl/stringops.py
+++ kcl/stringops.py
@@ -14,3 +14,5 @@ def hash_str(str_to_hash):
     assert len(str_to_hash) > 0
     return hashlib.sha1(str_to_hash.encode('utf-8')).hexdigest()
 
+def str_to_int(string):
+    return int.from_bytes(string.encode('utf8'), byteorder='little')
