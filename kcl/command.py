a0cbc4f41d942551c92e836593d213e15b2cbc9d
jakeogh <github.com@v6y.net>
2017-08-29 11:47:52 -0700

    auto-commit

--- kcl/command.py
+++ kcl/command.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 import subprocess
-from .printops import cprint
+from kcl.printops import eprint
 import os
 
 def run_command(command, verbose=False):
     if verbose:
-        cprint("command:", command)
+        eprint("command:", command)
     try:
         #output = subprocess.getoutput(command)
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
         if verbose:
-            cprint("output:", output)
+            eprint("output:", output)
         if b'error' in output.lower():
-            cprint("error in output.lower():", output.lower())
+            eprint("error in output.lower():", output.lower())
             os._exit(1)
         if b'warning' in output.lower():
-            cprint("warning in output.lower():", output.lower())
+            eprint("warning in output.lower():", output.lower())
             #os._exit(1)
     except subprocess.CalledProcessError as error:
-        cprint("exit code:", error.returncode, error.output)
+        eprint("exit code:", error.returncode, error.output)
         os._exit(1)
 
     return output
