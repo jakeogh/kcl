ccabfebdc29536c7dc86f2ab2dc52fdda4c5368a
jakeogh <github.com@v6y.net>
2017-10-08 10:14:48 -0700

    auto-commit

--- kcl/htmlops.py
+++ kcl/htmlops.py
@@ -5,6 +5,7 @@ import os
 from urllib.parse import urlparse
 import urllib.request
 import lxml.html
+from lxml.etree import ParserError
 import re
 from bs4 import BeautifulSoup
 from kcl.fileops import read_file_bytes_or_exit
@@ -51,7 +52,11 @@ def parse_html_to_dom(html):
 
 def extract_urls_lxml_with_link_text(html, url):
     url_list = []
-    dom = lxml.html.fromstring(html)
+    try:
+        dom = lxml.html.fromstring(html)
+    except ParserError:
+        return set([])
+
     dom.make_links_absolute(url)
     links = dom.cssselect('a')
     for link in links:
@@ -71,27 +76,12 @@ def extract_urls_lxml_with_link_text(html, url):
     return set(url_list)
 
 
-
-
 def extract_urls_lxml(html, url):
-    url_list = []
-    dom = lxml.html.fromstring(html)
-    dom.make_links_absolute(url)
-    links = dom.cssselect('a')
-    for link in links:
-        try:
-            if link.attrib['href'].startswith("javascript"):
-                pass
-            else:
-                try:
-                    current_url = link.attrib['href']
-                    if current_url != 'http://' and current_url != 'https://':
-                        url_list.append(link.attrib['href'])
-                except:
-                    pass
-        except:
-            pass
-    return set(url_list)
+    url_only_list = []
+    url_list = extract_urls_lxml_with_link_text(html=html, url=url)
+    for item in url_list:
+        url_only_list.append(item[0])
+    return set(url_only_list)
 
 def extract_urls_lxml_nofollow(html, url):
     url_list=[]
