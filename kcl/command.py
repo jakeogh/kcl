5c5da63c8c886cb025947de3ef0462d06c311bfe
jakeogh <github.com@v6y.net>
2017-11-04 00:02:33 -0700

    auto-commit

--- kcl/command.py
+++ kcl/command.py
@@ -9,14 +9,14 @@ def run_command(command, verbose=False):
     try:
         #output = subprocess.getoutput(command)
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
-        if verbose:
-            eprint("output:", output)
-        if b'error' in output.lower():
-            eprint("error in output.lower():", output.lower())
-            os._exit(1)
-        if b'warning' in output.lower():
-            eprint("warning in output.lower():", output.lower())
-            #os._exit(1)
+        #if verbose:
+        #    eprint("output:", output)
+        #if b'error' in output.lower():
+        #    eprint("error in output.lower():", output.lower())
+        #    os._exit(1)
+        #if b'warning' in output.lower():
+        #    eprint("warning in output.lower():", output.lower())
+        #    #os._exit(1)
     except subprocess.CalledProcessError as error:
         eprint("exit code:", error.returncode, error.output)
         os._exit(1)
