d38a9799072d8f5808716f6eafd9c8bc07948477
jakeogh <github.com@v6y.net>
2017-11-03 21:53:30 -0700

    auto-commit

--- kcl/hashops.py
+++ kcl/hashops.py
@@ -34,9 +34,9 @@ def generate_hash(data, verbose=False):
             temp_file.write(chunk)
             current_file_size = int(os.path.getsize(temp_file.name))
             if data_size:
-                eprint(data.url, temp_file.name, current_file_size, str(int((current_file_size/data_size)*100))+'%', end='\r', flush=True)
+                eprint(temp_file.name, current_file_size, str(int((current_file_size/data_size)*100))+'%', data.url, end='\r', flush=True)
             else:
-                eprint(data.url, temp_file.name, current_file_size, end='\r', flush=True)
+                eprint(temp_file.name, current_file_size, data.url, end='\r', flush=True)
         temp_file.close()
         if verbose: eprint('\n', end='')
         #eprint('finished writing temp_file: %s', temp_file.name)
