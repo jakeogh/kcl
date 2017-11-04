719d91c6aab3cc4d92e6d41546fdbbddba45a3c8
jakeogh <github.com@v6y.net>
2017-11-03 21:35:30 -0700

    auto-commit

--- kcl/hashops.py
+++ kcl/hashops.py
@@ -24,13 +24,19 @@ def generate_hash(data, verbose=False):
         if verbose:
             #import IPython; IPython.embed()
             eprint(data.url)
-            data_size = int(data.headers['Content-Length'])
+            try:
+                data_size = int(data.headers['Content-Length'])
+            except KeyError:
+                data_size = False
             eprint("data_size:", data_size)
         for chunk in data.iter_content(chunk_size):
             sha1.update(chunk)
             temp_file.write(chunk)
             current_file_size = int(os.path.getsize(temp_file.name))
-            eprint(temp_file.name, current_file_size, str(int((current_file_size/data_size)*100))+'%', end='\r', flush=True)
+            if data_size:
+                eprint(temp_file.name, current_file_size, str(int((current_file_size/data_size)*100))+'%', end='\r', flush=True)
+            else:
+                eprint(temp_file.name, current_file_size, end='\r', flush=True)
         temp_file.close()
         if verbose: eprint('\n', end='')
         #eprint('finished writing temp_file: %s', temp_file.name)
