67dec94e14605040a592cdc2e56310d843771156
jakeogh <github.com@v6y.net>
2017-11-25 14:46:30 -0700

    auto-commit

--- kcl/sqlalchemy/list_tables.py
+++ kcl/sqlalchemy/list_tables.py
@@ -3,7 +3,7 @@ from kcl.postgresqlops import get_engine
 import click
 
 @click.command()
-@click.argument(dbpath, nargs=1)
+@click.argument("dbpath", nargs=1)
 def list_tables(dbpath):
     ENGINE = get_engine(dbpath=dbpath)
     tables = set(ENGINE.table_names())
