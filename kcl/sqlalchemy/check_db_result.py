36cb846fdaddb25cd991dc1fe044addb4dd510b6
jakeogh <github.com@v6y.net>
2017-11-24 21:54:49 -0700

    auto-commit

--- kcl/sqlalchemy/check_db_result.py
+++ kcl/sqlalchemy/check_db_result.py
@@ -1,35 +1,7 @@
 #!/usr/bin/env python3
 
-#import pprint
-#pp = pprint.PrettyPrinter(indent=4)
-
-#from kcl.sqlalchemy.Filename import Filename
-#from anormbookmarker.TagClassConstructor import TagClassConstructor
-#Tag = TagClassConstructor(mapper_to_bookmark=Filename)
-#
-#from anormbookmarker.BookmarkClassConstructor import BookmarkClassConstructor
-#Bookmark = BookmarkClassConstructor(mapper_to_bookmark=Filename)
-#
-#from anormbookmarker.Alias import Alias
-#from anormbookmarker.Word import WordMisSpelling
-#from anormbookmarker.Word import Word
-#from anormbookmarker.Config import CONFIG
 from kcl.postgresqlops import get_engine
-#from kcl.postgresqlops import create_database_and_tables
-#from kcl.postgresqlops import create_session
 from kcl.postgresqlops import drop_database
-#from anormbookmarker.find_alias import ConflictingAliasError
-#from kcl.sqlalchemy.BaseMixin import BASE
-#
-#import logging
-#logger = logging.getLogger()
-#logger.setLevel(logging.CRITICAL)
-#logging.basicConfig()
-#logging.getLogger('sqlalchemy.engine').setLevel(logging.CRITICAL)
-#
-#create_database_and_tables(dbname=CONFIG.dbname, schema=BASE)
-#SESSION = create_session(dbname=CONFIG.dbname, multithread=False)
-
 
 def check_db_result(config, db_result, session):
     ENGINE = get_engine(dbpath=config.dbpath)
