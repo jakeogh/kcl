642a88d9bec8e9159dff7eb7c86f7a8b7c901e59
jakeogh <github.com@v6y.net>
2017-11-25 15:22:54 -0700

    auto-commit

--- kcl/postgresqlops.py
+++ kcl/postgresqlops.py
@@ -9,20 +9,26 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import ProgrammingError
 from .printops import eprint
 
-def drop_database(dbname):
+def drop_database(database):
+    dbpath = 'postgresql://postgres@localhost/postgres'
+    assert dbpath in database
+    dbname = database.split(dbpath)[-1]
     eprint("dropping database:", dbname)
-    with create_engine('postgresql://postgres@localhost/postgres',
-                       isolation_level='AUTOCOMMIT', echo=False).connect() as connection:
+    #with create_engine('postgresql://postgres@localhost/postgres',
+    with create_engine(dbpath, isolation_level='AUTOCOMMIT', echo=False).connect() as connection:
         connection.execute('DROP DATABASE ' + dbname)
 
-def create_database(dbname):
-    with create_engine('postgresql://postgres@localhost/postgres',
-                       isolation_level='AUTOCOMMIT', echo=False).connect() as connection:
+def create_database(database):
+    dbpath = 'postgresql://postgres@localhost/postgres'
+    assert dbpath in database
+    dbname = database.split(dbpath)[-1]
+    eprint("CREATE DATABASE:", dbname)
+    with create_engine(dbpath, isolation_level='AUTOCOMMIT', echo=False).connect() as connection:
         connection.execute('CREATE DATABASE ' + dbname)
 
-def create_database_and_tables(dbname, schema):
-    create_database(dbname=dbname)
-    create_tables(dbname=dbname, schema=schema)
+def create_database_and_tables(database, schema):
+    create_database(database=database)
+    create_tables(database=database, schema=schema)
 
 def install_extensions(dbname):
     with create_engine('postgresql://postgres@localhost/' + dbname,
@@ -30,44 +36,42 @@ def install_extensions(dbname):
         connection.execute('CREATE EXTENSION uint;')
 
 
-def delete_and_recreate_database(dbname):
+def delete_and_recreate_database(database):
     try:
-        drop_database(dbname)
+        drop_database(database=database)
     except ProgrammingError:
             pass #db didnt exist
     finally:
-        create_database(dbname)
+        create_database(database=database)
         #install_extensions(dbname)
 
 def start_database():
     os.system('sudo /etc/init.d/postgresql-9.6 start')
 
-def create_tables(dbname, schema):
-    temp_engine = create_engine("postgres://postgres@localhost/" + dbname, echo=False)
+def create_tables(database, schema):
+    #temp_engine = create_engine("postgres://postgres@localhost/" + dbname, echo=False)
+    temp_engine = create_engine(database, echo=False)
     schema.metadata.create_all(temp_engine)
 
-def create_session(dbname, multithread=False):
+def create_session(database, multithread=False):
     if not multithread:
-        ENGINE = create_engine("postgres://postgres@localhost/" + dbname,
-                               echo=False, poolclass=NullPool)
-        Session = scoped_session(sessionmaker(autocommit=False,
-                                              autoflush=False, bind=ENGINE))
+        #ENGINE = create_engine("postgres://postgres@localhost/" + dbname,
+        engine = create_engine(database, echo=False, poolclass=NullPool)
+        session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
     else:
-        ENGINE = create_engine("postgres://postgres@localhost/" + dbname,
-                               echo=False, pool_size=20, max_overflow=100)
-        Session = scoped_session(sessionmaker(autocommit=True,
-                                              autoflush=False, bind=ENGINE))
-    return Session
+        engine = create_engine(database, echo=False, pool_size=20, max_overflow=100)
+        session = scoped_session(sessionmaker(autocommit=True, autoflush=False, bind=engine))
+    return session
 
 
-def delete_and_recreate_database_and_session(dbname, schema):
-    delete_and_recreate_database(dbname)
-    create_tables(dbname, schema)
-    return create_session(dbname)
+def delete_and_recreate_database_and_session(database, schema):
+    delete_and_recreate_database(database=database)
+    create_tables(database=database, schema=schema)
+    return create_session(database=database)
 
-def get_engine(dbpath):
-    assert isinstance(dbpath, str)
-    engine = create_engine(dbpath, echo=False)
+def get_engine(database):
+    assert isinstance(database, str)
+    engine = create_engine(database=database, echo=False)
     return engine
 
 
