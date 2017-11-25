c79d4a006fc882e18268bd5b83113615637ec27f
jakeogh <github.com@v6y.net>
2017-11-25 15:49:44 -0700

    auto-commit

--- kcl/sqlalchemy/list_tables.py
+++ kcl/sqlalchemy/list_tables.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 from kcl.postgresqlops import get_engine
+from kcl.sqlalchemy.BaseMixin import BASE
+from pprint import pprint
 import click
 
 @click.command()
 @click.argument("database", nargs=1)
-def list_tables(database):
-    ENGINE = get_engine(database=database)
-    tables = set(ENGINE.table_names())
-    print("tables:", tables)
-    assert tables
-
+@click.option("--verbose", is_flag=True)
+def list_tables(database, verbose):
+    engine = get_engine(database=database)
+    if verbose:
+        BASE.metadata.reflect(engine)
+        table_names = BASE.metadata.tables.keys()
+        for table in table_names:
+            pprint(BASE.metadata.tables[table])
+    else:
+        tables = engine.table_names()
+        print("tables:", tables)
