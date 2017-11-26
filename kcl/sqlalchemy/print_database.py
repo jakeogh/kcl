b3cb48e055edaa3f10e0d835394fd073c08d79ee
jakeogh <github.com@v6y.net>
2017-11-26 14:49:10 -0700

    auto-commit

---
+++ kcl/sqlalchemy/print_database.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# MIT License
+
+import pprint
+import click
+from sqlalchemy import inspect as sqlalchemy_inspect
+
+@click.command()
+@click.argument('session')
+def print_database(session):
+    inspector = sqlalchemy_inspect(session.bind)
+    for table_name in sorted(inspector.get_table_names()):
+        print('\n' + table_name + ':')
+        columns = inspector.get_columns(table_name)
+        for column in columns:
+            print("%s, " % column['name'], end='')
+        print('\n', end='')
+        select_statement = "select * from " + table_name + ";"
+        pprint.pprint(session.execute(select_statement).fetchall())
