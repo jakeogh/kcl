bc8fb0478155a5c3755757675b31b51a815c4118
jakeogh <github.com@v6y.net>
2017-11-28 00:40:02 -0700

    auto-commit

--- kcl/sqlalchemy/print_database.py
+++ kcl/sqlalchemy/print_database.py
@@ -9,10 +9,14 @@ from kcl.sqlalchemy.create_session import create_session
 
 @click.command()
 @click.argument('database')
-def print_database(database):
+@click.option('--table', type=str, default=False)
+def print_database(database, table):
     session = create_session(database=database)
     inspector = sqlalchemy_inspect(session.bind)
     for table_name in sorted(inspector.get_table_names()):
+        if table:
+            if table_name != table:
+                continue
         print('\n' + table_name + ':')
         columns = inspector.get_columns(table_name)
         for column in columns:
