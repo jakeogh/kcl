53669a9bd7b983df04313c0a56b2e7015cc617e3
jakeogh <github.com@v6y.net>
2017-12-01 15:32:27 -0700

    auto-commit

--- kcl/sqlalchemy/create_database.py
+++ kcl/sqlalchemy/create_database.py
@@ -2,12 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from sqlalchemy import create_engine
-from sqlalchemy.pool import NullPool
-from sqlalchemy.orm import scoped_session
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import ProgrammingError
 from kcl.printops import eprint
-from kcl.sqlalchemy.create_session import create_session
 
 def create_database(database):
     dbpath = 'postgresql://postgres@localhost/'
