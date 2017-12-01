67e6e12d48640e17cc8b4318deb79cd06d11d19c
jakeogh <github.com@v6y.net>
2017-12-01 15:53:34 -0700

    auto-commit

--- kcl/sqlalchemy/create_tables.py
+++ kcl/sqlalchemy/create_tables.py
@@ -2,12 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from sqlalchemy import create_engine
-from sqlalchemy.pool import NullPool
-from sqlalchemy.orm import scoped_session
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import ProgrammingError
-from kcl.printops import eprint
-from kcl.sqlalchemy.create_session import create_session
 
 def create_tables(database, schema):
     temp_engine = create_engine(database, echo=False)
