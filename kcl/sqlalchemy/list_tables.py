37f3ea92cee9dc0bdb6e4ada242308f1dca9f6fe
jakeogh <github.com@v6y.net>
2017-12-17 22:55:57 -0700

    auto-commit

--- kcl/sqlalchemy/list_tables.py
+++ kcl/sqlalchemy/list_tables.py
@@ -13,19 +13,19 @@ from kcl.sqlalchemy.self_contained_session import self_contained_session
 @click.pass_obj
 def list_tables(config, database, verbose, contents):
     with self_contained_session(config.database) as session:
-    engine = session.bind
-    BASE.metadata.reflect(engine)
-    table_names = BASE.metadata.tables.keys()
-    for table in table_names:
-        print(" ")
-        table_instance = BASE.metadata.tables[table]
-        if verbose:
-            pprint(table_instance)
-        else:
-            print(table)
-        if contents:
-            try:
-                pprint(session.query(table_instance).all())
-            except ProgrammingError:
-                session.rollback()
+        engine = session.bind
+        BASE.metadata.reflect(engine)
+        table_names = BASE.metadata.tables.keys()
+        for table in table_names:
+            print(" ")
+            table_instance = BASE.metadata.tables[table]
+            if verbose:
+                pprint(table_instance)
+            else:
+                print(table)
+            if contents:
+                try:
+                    pprint(session.query(table_instance).all())
+                except ProgrammingError:
+                    session.rollback()
 
