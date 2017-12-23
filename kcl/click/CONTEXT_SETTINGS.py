6ca161bf34d11d6350d72c122b7b3caaf288d1c9
jakeogh <github.com@v6y.net>
2017-12-23 02:55:35 -0700

    auto-commit

--- kcl/click/CONTEXT_SETTINGS.py
+++ kcl/click/CONTEXT_SETTINGS.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import shutil
+
 # https://github.com/mitsuhiko/click/issues/441
 CONTEXT_SETTINGS = \
     dict(help_option_names=['--help'], terminal_width=shutil.get_terminal_size((80, 20)).columns)
