2318c8360174e6f24815344268a9593af461b879
jakeogh <github.com@v6y.net>
2017-12-23 20:08:11 -0700

    auto-commit

--- kcl/sqlalchemy/model/Filename.py
+++ kcl/sqlalchemy/model/Filename.py
@@ -32,6 +32,6 @@ class Filename(BASE):
         return result
 
     def __repr__(self):
-        return self.filename
+        return str(self.filename)
     def __bytes__(self):
         return self.filename
