a9481ec38a9eace26ecc8901af3fd69d153282a3
jakeogh <github.com@v6y.net>
2017-12-23 18:27:12 -0700

    auto-commit

--- kcl/sqlalchemy/model/Path.py
+++ kcl/sqlalchemy/model/Path.py
@@ -17,18 +17,14 @@ class Path(BASE):
     UNIX paths can be anything but NULL
     max path length is undefined:
         http://insanecoding.blogspot.com/2007/11/pathmax-simply-isnt.html
-    all paths must start with /
     '''
     id = Column(Integer, primary_key=True)
-    #path_constraint = "position('\\x00' in path) = 0 and position('\\x2f' in path) = 0"
     path_constraint = "position('\\x00' in path) = 0" #todo test, add constraint to replace asserts
     path = Column(LargeBinary(), CheckConstraint(path_constraint), unique=True, nullable=False, index=True)
 
     @classmethod
     def construct(cls, session, path):
         assert isinstance(path, bytes)
-        assert path.startswith(b'/')
-        #print("Path.construct()")
         result = get_one_or_create(session, Path, path=path)
         return result
 
