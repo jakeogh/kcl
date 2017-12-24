d31abc664688485b08ce155192fd437af43d70cb
jakeogh <github.com@v6y.net>
2017-12-23 20:13:05 -0700

    auto-commit

---
+++ kcl/sqlalchemy/ipython.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import click
+from kcl.sqlalchemy.BaseMixin import BASE
+from kcl.sqlalchemy.self_contained_session import self_contained_session
+
+@click.command()
+@click.pass_obj
+def ipython(config):
+     with self_contained_session(config.database) as session:
+        BASE.metadata.create_all(session.bind)
+        import IPython; IPython.embed()
