892b55afe6e5623cde5d495a34cab4e8dcb0109a
jakeogh <github.com@v6y.net>
2017-12-23 21:50:48 -0700

    auto-commit

---
+++ kcl/sqlalchemy/base_test_enviroment.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+from sqlalchemy_utils.functions import create_database
+from kcl.sqlalchemy.delete_database import delete_database
+from kcl.sqlalchemy.check_db_result import check_db_result
+from kcl.sqlalchemy.BaseMixin import BASE
+from kcl.sqlalchemy.self_contained_session import self_contained_session
+from kcl.printops import eprint
+
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.CRITICAL)
+logging.basicConfig()
+logging.getLogger('sqlalchemy.engine').setLevel(logging.CRITICAL)
+
+CONFIG.database = CONFIG.database_timestamp
+create_database(CONFIG.database)
