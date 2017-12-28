130294bab10ee4e9c3170acc0fcc0a2ada2a6a22
jakeogh <github.com@v6y.net>
2017-12-28 02:04:19 -0700

    auto-commit

--- kcl/kcl.py
+++ kcl/kcl.py
@@ -2,7 +2,10 @@
 
 from kcl.sqlalchemy.clickapp.ClickApp import ClickApp
 from kcl.model.Config import CONFIG
+from kcl.sqlalchemy.ipython import ipython
 
 CA = ClickApp(config=CONFIG)
 clickapp = CA.clickapp
 clickapp.help = CONFIG.appname + " interface"
+
+clickapp.add_command(ipython)
