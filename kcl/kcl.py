43fe67527bc23c1bfa8dd258a9bfe64cd85b236a
jakeogh <github.com@v6y.net>
2017-12-27 22:21:54 -0700

    auto-commit

--- kcl/kcl.py
+++ kcl/kcl.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
-from kcl.sqlalchemy.clickapp.clickapp import clickapp
+#from kcl.sqlalchemy.clickapp.clickapp import clickapp
+from kcl.sqlalchemy.clickapp.ClickApp import ClickApp
 from kcl.sqlalchemy.model.BaseConfig import BASECONFIG as CONFIG
+
+CA = ClickApp(config=config)
+clickapp = CA.clickapp
