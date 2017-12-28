5f826db199dd01e4ea6817b7b36c8d0a6c4a1f7d
jakeogh <github.com@v6y.net>
2017-12-28 02:34:25 -0700

    auto-commit

--- kcl/sqlalchemy/clickapp/ClickApp.py
+++ kcl/sqlalchemy/clickapp/ClickApp.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import builtins
+CONFIG = builtins.config
+
 import click
 from sqlalchemy_utils.functions import database_exists
 from sqlalchemy_utils.functions import create_database
@@ -10,18 +12,8 @@ from kcl.sqlalchemy.self_contained_session import self_contained_session
 from kcl.sqlalchemy.BaseMixin import BASE
 from kcl.sqlalchemy.ipython import ipython
 from kcl.click.CONTEXT_SETTINGS import CONTEXT_SETTINGS
-#from kcl.sqlalchemy.model.BaseConfig import BASECONFIG as CONFIG
-
-CONFIG = builtins.config
 
 __version__ = 0.01
-
-#class ClickApp():
-#    def __init__(self, config):
-#        self.config = config
-
-#def clickapp(self, ctx, verbose, database, temp_database, delete_database):
-
 
 @click.group(context_settings=CONTEXT_SETTINGS, help="boilerplate orm interface")
 @click.option('--verbose', is_flag=True)
