814ae5bda52471d7a25f9b1ae848b91e057b37f0
jakeogh <github.com@v6y.net>
2017-12-28 01:56:45 -0700

    auto-commit

--- kcl/kcl.py
+++ kcl/kcl.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 
-#from kcl.sqlalchemy.clickapp.clickapp import clickapp
 from kcl.sqlalchemy.clickapp.ClickApp import ClickApp
-#from kcl.sqlalchemy.model.BaseConfig import BASECONFIG as CONFIG
 from kcl.model.Config import CONFIG
 
 CA = ClickApp(config=CONFIG)
 clickapp = CA.clickapp
-#print(clickapp.__doc__)
-#clickapp.__doc__ == "here"
 
