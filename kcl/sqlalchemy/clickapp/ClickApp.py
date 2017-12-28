b0a61cde1ef25a0258620e542473c57cf404678f
jakeogh <github.com@v6y.net>
2017-12-28 01:58:15 -0700

    auto-commit

--- kcl/sqlalchemy/clickapp/ClickApp.py
+++ kcl/sqlalchemy/clickapp/ClickApp.py
@@ -26,10 +26,6 @@ class ClickApp():
     @click.option('--delete-database', is_flag=True, required=False)
     @click.pass_context
     def clickapp(self, ctx, verbose, database, temp_database, delete_database):
-        #clickapp.__doc__ = "orm interface"
-        #__doc__ = self.config.appname + "orm interface"
-        #self.__doc__ = self.config.appname + "orm interface"
-        #''' clickapp orm interface'''
         if database:
             if temp_database:
                 eprint("Error: --database and --temp-database are mutually exclusive.")
