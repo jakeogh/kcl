d88e5e8ed1a7f53375a9a5776207a56d8946d7d0
jakeogh <github.com@v6y.net>
2017-12-29 20:19:48 -0700

    auto-commit

---
+++ kcl/sqlalchemy/clickapp/print_database.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import click
+import builtins
+CONFIG = builtins.config
+from kcl.sqlalchemy.visualization import print_database as kcl_print_database
+
+@click.command()
+@click.option('--table', type=str, default=False)
+@click.pass_context
+def print_database(ctx, table):
+    ctx.invoke(kcl_print_database, database=CONFIG.database, table=table)
+
