0a89c6fd56db91b41762fe9454c66c466d727825
jakeogh <github.com@v6y.net>
2017-12-29 22:44:20 -0700

    auto-commit

--- kcl/kcl.py
+++ kcl/kcl.py
@@ -4,12 +4,11 @@ import builtins
 from kcl.model.Config import CONFIG
 builtins.config = CONFIG
 
-
-from kcl.sqlalchemy.clickapp.clickapp import clickapp
+from kcl.sqlalchemy.clickapp.clickapp import clickapp as kcl
 from kcl.sqlalchemy.ipython import ipython
 #from kcl.sqlalchemy.visualization.sa_display import sa_display
 
-clickapp.help = CONFIG.appname + " interface"
-clickapp.add_command(ipython)
+kcl.help = CONFIG.appname + " interface"
+kcl.add_command(ipython)
 #clickapp.add_command(sa_display)
 
