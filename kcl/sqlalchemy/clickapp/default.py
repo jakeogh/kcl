3a80e8f40edf4e7881e933c420e108291ee28805
jakeogh <github.com@v6y.net>
2017-12-29 21:40:15 -0700

    auto-commit

---
+++ kcl/sqlalchemy/clickapp/default.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from kcl.sqlalchemy.clickapp.print_database import print_database
+from kcl.sqlalchemy.clickapp.test import test
+from kcl.sqlalchemy.clickapp.show_config import show_config
+from kcl.sqlalchemy.ipython import ipython
