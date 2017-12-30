a35c4ae1327f778f294e0d17ee4a1f6df1a38d0f
jakeogh <github.com@v6y.net>
2017-12-29 22:57:45 -0700

    auto-commit

--- kcl/sqlalchemy/clickapp/default.py
+++ kcl/sqlalchemy/clickapp/default.py
@@ -3,8 +3,14 @@
 import builtins
 CONFIG = builtins.config
 
-from kcl.sqlalchemy.clickapp.print_database import print_database
-from kcl.sqlalchemy.clickapp.test import test
-from kcl.sqlalchemy.clickapp.show_config import show_config
-from kcl.sqlalchemy.ipython import ipython
+from kcl.sqlalchemy.clickapp.ipython import ipython
 CONFIG.appobject.add_command(ipython)
+
+from kcl.sqlalchemy.clickapp.print_database import print_database
+CONFIG.appobject.add_command(print_database)
+
+from kcl.sqlalchemy.clickapp.show_config import show_config
+CONFIG.appobject.add_command(show_config)
+
+from kcl.sqlalchemy.clickapp.test import test
+CONFIG.appobject.add_command(test)
