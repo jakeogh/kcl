3315a4f33b24e33e82126f18520596846da06671
jakeogh <github.com@v6y.net>
2017-12-31 03:08:05 -0700

    auto-commit

--- kcl/sqlalchemy/clickapp/cli/debug/debug.py
+++ kcl/sqlalchemy/clickapp/cli/debug/debug.py
@@ -2,9 +2,11 @@
 
 import click
 from kcl.sqlalchemy.clickapp.cli.debug.ipython import ipython
+from kcl.sqlalchemy.clickapp.cli.debug.start_pudb import start_pudb
 
 @click.group()
 def debug():
     pass
 
 debug.add_command(ipython)
+debug.add_command(start_pudb, name'pudb')
