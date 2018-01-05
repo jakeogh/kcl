ee9d7ef7e87c8cab8358af839c19102ffb8f39fc
jakeogh <github.com@v6y.net>
2018-01-04 23:39:01 -0700

    auto-commit

--- kcl/sqlalchemy/clickapp/cli/database/database.py
+++ kcl/sqlalchemy/clickapp/cli/database/database.py
@@ -2,6 +2,7 @@
 
 import click
 from kcl.sqlalchemy.clickapp.cli.database.tables import tables
+from kcl.sqlalchemy.clickapp.cli.database.fields import fields
 from kcl.sqlalchemy.clickapp.cli.database.print_database import print_database
 
 @click.group()
@@ -9,4 +10,5 @@ def database():
     pass
 
 database.add_command(tables)
+database.add_command(fields)
 database.add_command(print_database, name="print")
