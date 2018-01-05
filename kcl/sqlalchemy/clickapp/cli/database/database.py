9a7487c119245eaf358ceae8ed7c7b7b9186f0d5
jakeogh <github.com@v6y.net>
2018-01-04 23:22:53 -0700

    auto-commit

--- kcl/sqlalchemy/clickapp/cli/database/database.py
+++ kcl/sqlalchemy/clickapp/cli/database/database.py
@@ -2,9 +2,11 @@
 
 import click
 from kcl.sqlalchemy.clickapp.cli.database.tables import tables
+from kcl.sqlalchemy.clickapp.cli.database.print_database import print_database
 
 @click.group()
 def database():
     pass
 
 database.add_command(tables)
+database.add_command(print_database, name="print")
