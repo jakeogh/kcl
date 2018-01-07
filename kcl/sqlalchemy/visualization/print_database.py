b52ea83a68f1e7050d11e8d337d8d9497dec4bc8
jakeogh <github.com@v6y.net>
2018-01-06 20:31:19 -0700

    auto-commit

--- kcl/sqlalchemy/visualization/print_database.py
+++ kcl/sqlalchemy/visualization/print_database.py
@@ -26,7 +26,7 @@ def print_database(database, table, contents):
                 if table_name != table:
                     continue
             else:
-                print('\n' + table_name + ':'.lpad(max_length_table_name), end='')
+                print('\n' + table_name + ':'.ljust(max_length_table_name), end='')
             columns = inspector.get_columns(table_name)
             for column in columns:
                 if table:
