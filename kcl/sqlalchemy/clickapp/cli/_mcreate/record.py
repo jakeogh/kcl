cb4953ca1f79df413f04be7344576ea444608a27
jakeogh <github.com@v6y.net>
2018-01-06 18:07:23 -0700

    auto-commit

--- kcl/sqlalchemy/clickapp/cli/_mcreate/record.py
+++ kcl/sqlalchemy/clickapp/cli/_mcreate/record.py
@@ -12,8 +12,10 @@ from kcl.sqlalchemy.BaseMixin import BASE
 def record(config, classname):
     with self_contained_session(config.database) as session:
         BASE.metadata.create_all(session.bind)
+        class_path = '.'.join(classname.split('.')[0:-1])
+        classname = classname.split('.')[-1]
         globals()[classname] = __import__(classname, globals=globals(), locals=locals(), fromlist=[classname], level=0)
-        cmd_to_eval = classname + ' = ' + classname + '.' + classname
+        cmd_to_eval = classname + ' = ' + class_path + '.' + classname
         print(cmd_to_eval)
         exec(cmd_to_eval)
         #filerecord = FileRecord.construct(session=session, inpath=path)
