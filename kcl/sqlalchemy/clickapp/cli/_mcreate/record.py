e9042c440ceb70ffd04094c48c8d2ec2239191b3
jakeogh <github.com@v6y.net>
2018-01-06 18:52:19 -0700

    auto-commit

--- kcl/sqlalchemy/clickapp/cli/_mcreate/record.py
+++ kcl/sqlalchemy/clickapp/cli/_mcreate/record.py
@@ -21,7 +21,7 @@ def record(ctx, class_name):
         class_name = class_name.split('.')[-1]
         full_class_path = class_path + '.' + class_name
         globals()[class_name] = getattr(__import__(full_class_path, globals=globals(), locals=locals(), fromlist=[class_name], level=0), class_name)
-        print(d)
+        #print(d)
         new_object = globals()[class_name].construct(session=session, **ctx.kwargs)
         import IPython; IPython.embed()
         #session.commit()
