ed41119578eeeb3d3ddb2541cdb26a747cef978c
jakeogh <github.com@v6y.net>
2018-01-06 18:13:08 -0700

    auto-commit

--- kcl/sqlalchemy/clickapp/cli/_mcreate/record.py
+++ kcl/sqlalchemy/clickapp/cli/_mcreate/record.py
@@ -16,7 +16,7 @@ def record(config, class_name):
         class_name = class_name.split('.')[-1]
         full_class_path = class_path + '.' + class_name
         globals()[class_name] = __import__(full_class_path, globals=globals(), locals=locals(), fromlist=[class_name], level=0)
-        cmd_to_eval = class_name + ' = ' + class_path + '.' + class_name
+        cmd_to_eval = class_name + ' = ' + class_name + '.' + class_name
         print(cmd_to_eval)
         exec(cmd_to_eval)
         #filerecord = FileRecord.construct(session=session, inpath=path)
