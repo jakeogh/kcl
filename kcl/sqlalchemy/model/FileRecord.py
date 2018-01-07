dfab8fcf56557c69458a06e220228fdefbc8f40a
jakeogh <github.com@v6y.net>
2018-01-07 04:29:30 -0700

    auto-commit

--- kcl/sqlalchemy/model/FileRecord.py
+++ kcl/sqlalchemy/model/FileRecord.py
@@ -130,7 +130,8 @@ class FileRecord(BASE):
 
     def __repr__(self):
         #return "<FileRecord(id=%s path=%s filename=%s byteshash=%s)>" % (str(self.id), str(self.path), str(self.filename), str(self.byteshash))
-        return "<FileRecord(id=%s path=%s filename=%s)>" % (str(self.id), str(self.path), str(self.filename))
+        #return "<FileRecord(id=%s path=%s filename=%s)>" % (str(self.id), str(self.path), str(self.filename))
+        return "<FileRecord(file=%s)>" % (str(self.file))
 
     @hybrid_property
     def file(self):
