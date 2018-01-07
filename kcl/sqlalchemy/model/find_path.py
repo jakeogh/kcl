dcacc7e47b790d3337b01d858aa2e2efd4cf1012
jakeogh <github.com@v6y.net>
2018-01-06 21:26:29 -0700

    auto-commit

--- kcl/sqlalchemy/model/find_path.py
+++ kcl/sqlalchemy/model/find_path.py
@@ -15,15 +15,13 @@ def find_path(session, path):
     it's reasonable to return the path if it's found beacuse unlike an alias, a path cant point to
     the wrong thing. returning a "duplicate" alias only makes sense if it's pointing to the same path.
     '''
-    corrected_path = path
     possible_path_set = set([])
     assert isinstance(path, bytes)
-
-    corrected_path = path
-    corrected_path_split = corrected_path.split(b'/')
+    path_split = path.split(b'/')
 
     try:
-        for index, filename in enumerate(corrected_path_split):
+        for index, filename in enumerate(path_split):
+            ceprint(index, filename)
             current_filename = session.query(Filename).filter_by(filename=filename).one()
             current_pathfilename_list = session.query(PathFilename).filter_by(filename=current_filename, position=index).all()
             if current_pathfilename_list:
@@ -43,9 +41,9 @@ def find_path(session, path):
                 if len(possible_path_set) == 1:
                     last_path = list(possible_path_set)[0]
                     last_path_text = str(last_path)
-                    if last_path_text == corrected_path:
+                    if last_path_text == path:
                         return last_path
                     return False
     except NoResultFound: # any failed query
-        #print("NoResultFound returning False")
+        print("NoResultFound returning False")
         return False
