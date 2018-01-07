5415755f42733b29e05d22eed9aa8b4986488a74
jakeogh <github.com@v6y.net>
2018-01-06 21:30:50 -0700

    auto-commit

--- kcl/sqlalchemy/model/find_path.py
+++ kcl/sqlalchemy/model/find_path.py
@@ -22,16 +22,16 @@ def find_path(session, path):
     try:
         for index, filename in enumerate(path_split):
             ceprint(index, filename)
-            current_filename = session.query(Filename).filter_by(filename=filename).one()
-            current_pathfilename_list = session.query(PathFilename).filter_by(filename=current_filename, position=index).all()
-            if current_pathfilename_list:
-                current_pathfilename_list_path_set = set([pathfilename.path for pathfilename in current_pathfilename_list])
+            filename = session.query(Filename).filter_by(filename=filename).one()
+            pathfilename_list = session.query(PathFilename).filter_by(filename=filename, position=index).all()
+            if pathfilename_list:
+                pathfilename_list_path_set = set([pathfilename.path for pathfilename in pathfilename_list])
                 if not possible_path_set:
-                    possible_path_set = current_pathfilename_list_path_set
+                    possible_path_set = pathfilename_list_path_set
                 else:
-                    possible_path_set = possible_path_set & current_pathfilename_list_path_set
-                for pathfilename in current_pathfilename_list:
-                    if index == 0: # only add paths that start with the correct filename
+                    possible_path_set = possible_path_set & pathfilename_list_path_set
+                for pathfilename in pathfilename_list:
+                    if index == 0:  # only add paths that start with the correct filename
                         possible_path_set.add(pathfilename.path)
                     else:
                         if pathfilename.path not in possible_path_set:
@@ -44,6 +44,6 @@ def find_path(session, path):
                     if last_path_text == path:
                         return last_path
                     return False
-    except NoResultFound: # any failed query
+    except NoResultFound:  # any failed query
         print("NoResultFound returning False")
         return False
