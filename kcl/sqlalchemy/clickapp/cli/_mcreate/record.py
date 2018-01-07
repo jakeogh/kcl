62160a5d67f1510547ed4500f7e29d22652bf57a
jakeogh <github.com@v6y.net>
2018-01-06 18:10:42 -0700

    auto-commit

--- kcl/sqlalchemy/clickapp/cli/_mcreate/record.py
+++ kcl/sqlalchemy/clickapp/cli/_mcreate/record.py
@@ -7,15 +7,16 @@ from kcl.sqlalchemy.BaseMixin import BASE
 
 
 @click.command()
-@click.argument('classname', type=str, nargs=1)
+@click.argument('class_name', type=str, nargs=1)
 @click.pass_obj
-def record(config, classname):
+def record(config, class_name):
     with self_contained_session(config.database) as session:
         BASE.metadata.create_all(session.bind)
-        class_path = '.'.join(classname.split('.')[0:-1])
-        classname = classname.split('.')[-1]
-        globals()[classname] = __import__(classname, globals=globals(), locals=locals(), fromlist=[classname], level=0)
-        cmd_to_eval = classname + ' = ' + class_path + '.' + classname
+        class_path = '.'.join(class_name.split('.')[0:-1])
+        class_name = class_name.split('.')[-1]
+        full_class_path = class_path + '.' + class_name
+        globals()[class_name] = __import__(full_class_path, globals=globals(), locals=locals(), fromlist=[class_name], level=0)
+        cmd_to_eval = class_name + ' = ' + class_path + '.' + class_name
         print(cmd_to_eval)
         exec(cmd_to_eval)
         #filerecord = FileRecord.construct(session=session, inpath=path)
