6416674e75996e4ae77bd9a1b11ba8dbb3460a0d
jakeogh <github.com@v6y.net>
2018-01-07 00:53:47 -0700

    auto-commit

--- kcl/sqlalchemy/model/find_path.py
+++ kcl/sqlalchemy/model/find_path.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm.exc import NoResultFound
 from .Filename import Filename
 from .PathFilename import PathFilename
 from kcl.printops import ceprint
+from kcl.printops import eprint
 
 def find_path(session, path):
     ceprint("path:", path)
@@ -20,39 +21,47 @@ def find_path(session, path):
     path_split = path.split(b'/')
     assert path_split
 
+    if path_split[-1] == b'':
+        eprint("special case for /")
+
     try:
         for index, filename in enumerate(path_split):
             ceprint(index, filename)
             filename = session.query(Filename).filter_by(filename=filename).one()
             ceprint("filename:", filename)
-            pathfilename_list = session.query(PathFilename).filter_by(filename=filename, position=index).all()
-            ceprint("pathfilename_list:")
-            for pf in pathfilename_list:
+
+            pathfilenames = session.query(PathFilename).filter_by(filename=filename, position=index).all()
+            ceprint("pathfilenames:")
+            for pf in pathfilenames:
                 ceprint(pf)
-            ceprint("pathfilename_list:", pathfilename_list)
-            if pathfilename_list:
-                pathfilename_list_path_set = set([pathfilename.filename for pathfilename in pathfilename_list])
-                ceprint("pathfilename_list_path_set:", pathfilename_list_path_set)
-                if not possible_path_set:
-                    possible_path_set = pathfilename_list_path_set
-                    ceprint("*possible_path_set:", possible_path_set)
+
+            paths_that_match_filename = set([pathfilename.path for pathfilename in pathfilenames])
+            ceprint("paths_that_match_filename:", paths_that_match_filename)
+            for pf in paths_that_match_filename:
+                ceprint(pf)
+
+
+
+            if len(possible_path_set) == 0:
+                possible_path_set = paths_that_match_filename
+                ceprint("*possible_path_set:", possible_path_set)
+            else:
+                possible_path_set = possible_path_set & paths_that_match_filename
+                ceprint("possible_path_set:", possible_path_set)
+            for pathfilename in pathfilenames:
+                if index == 0:  # only add paths that start with the correct filename
+                    possible_path_set.add(pathfilename.path)
                 else:
-                    possible_path_set = possible_path_set & pathfilename_list_path_set
-                    ceprint("possible_path_set:", possible_path_set)
-                for pathfilename in pathfilename_list:
-                    if index == 0:  # only add paths that start with the correct filename
-                        possible_path_set.add(pathfilename.path)
-                    else:
-                        if pathfilename.path not in possible_path_set:
-                            return False
-                if not possible_path_set:
-                    return False
-                if len(possible_path_set) == 1:
-                    last_path = list(possible_path_set)[0]
-                    last_path_text = str(last_path)
-                    if last_path_text == path:
-                        return last_path
-                    return False
+                    if pathfilename.path not in possible_path_set:
+                        return False
+            if not possible_path_set:
+                return False
+            if len(possible_path_set) == 1:
+                last_path = list(possible_path_set)[0]
+                last_path_text = str(last_path)
+                if last_path_text == path:
+                    return last_path
+                return False
     except NoResultFound:  # any failed query
         ceprint("NoResultFound returning False")
         return False
