f36c92975a667318db1525d1df2ede9accad5552
jakeogh <github.com@v6y.net>
2018-01-06 18:35:10 -0700

    auto-commit

--- kcl/sqlalchemy/clickapp/cli/_mcreate/record.py
+++ kcl/sqlalchemy/clickapp/cli/_mcreate/record.py
@@ -10,15 +10,18 @@ CONTEXT_SETTINGS['allow_extra_args'] = True
 
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.argument('class_name', type=str, nargs=1)
-@click.pass_obj
-def record(config, class_name):
-    with self_contained_session(config.database) as session:
+@click.pass_context
+def record(ctx, class_name):
+    with self_contained_session(ctx.obj.database) as session:
         BASE.metadata.create_all(session.bind)
+        d = dict()
+        for item in ctx.args:
+            d.update([item.split('=')])
         class_path = '.'.join(class_name.split('.')[0:-1])
         class_name = class_name.split('.')[-1]
         full_class_path = class_path + '.' + class_name
         globals()[class_name] = getattr(__import__(full_class_path, globals=globals(), locals=locals(), fromlist=[class_name], level=0), class_name)
-        new_object = globals()[class_name].construct(session=session, **kwargs)
+        new_object = globals()[class_name].construct(session=session, d)
         import IPython; IPython.embed()
         #session.commit()
         #print(bytes(filerecord))
