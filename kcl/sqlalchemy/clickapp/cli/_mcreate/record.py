a8730ea7e5048cc88e1377b0e1dc3c9fac57546d
jakeogh <github.com@v6y.net>
2018-01-06 18:28:41 -0700

    auto-commit

--- kcl/sqlalchemy/clickapp/cli/_mcreate/record.py
+++ kcl/sqlalchemy/clickapp/cli/_mcreate/record.py
@@ -4,9 +4,11 @@ import click
 from kcl.sqlalchemy.self_contained_session import self_contained_session
 #from kcl.sqlalchemy.model.FileRecord import FileRecord
 from kcl.sqlalchemy.BaseMixin import BASE
+from kcl.click.CONTEXT_SETTINGS import CONTEXT_SETTINGS
+CONTEXT_SETTINGS['ignore_unknown_options'] = True
+CONTEXT_SETTINGS['allow_extra_args'] = True
 
-
-@click.command()
+@click.command(context_settings=CONTEXT_SETTINGS)
 @click.argument('class_name', type=str, nargs=1)
 @click.pass_obj
 def record(config, class_name):
@@ -15,9 +17,8 @@ def record(config, class_name):
         class_path = '.'.join(class_name.split('.')[0:-1])
         class_name = class_name.split('.')[-1]
         full_class_path = class_path + '.' + class_name
-        #globals()[class_name] = __import__(full_class_path, globals=globals(), locals=locals(), fromlist=[class_name], level=0)
         globals()[class_name] = getattr(__import__(full_class_path, globals=globals(), locals=locals(), fromlist=[class_name], level=0), class_name)
-        new_object = globals()[class_name].construct()
+        new_object = globals()[class_name].construct(session=session, **kwargs)
         import IPython; IPython.embed()
         #session.commit()
         #print(bytes(filerecord))
