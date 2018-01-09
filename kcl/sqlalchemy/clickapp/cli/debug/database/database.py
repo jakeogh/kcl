2ea4fff355780eb2174f558d70f48ed92d4e7845
jakeogh <github.com@v6y.net>
2018-01-09 06:02:13 -0700

    auto-commit

--- kcl/sqlalchemy/clickapp/cli/debug/database/database.py
+++ kcl/sqlalchemy/clickapp/cli/debug/database/database.py
@@ -4,6 +4,8 @@ import click
 from kcl.sqlalchemy.clickapp.cli.debug.database.tables import tables
 from kcl.sqlalchemy.clickapp.cli.debug.database.fields import fields
 from kcl.sqlalchemy.clickapp.cli.debug.database.print_database import print_database
+from kcl.sqlalchemy.clickapp.cli.debug._count._count import _count
+from kcl.sqlalchemy.clickapp.cli.debug._list._list import _list
 
 @click.group()
 def database():
@@ -12,3 +14,5 @@ def database():
 database.add_command(tables)
 database.add_command(fields)
 database.add_command(print_database, name="print")
+database.add_command(_list, name="list")
+database.add_command(_count, name="count")
--- kcl/sqlalchemy/clickapp/cli/debug/debug.py
+++ kcl/sqlalchemy/clickapp/cli/debug/debug.py
@@ -5,16 +5,14 @@ from kcl.sqlalchemy.clickapp.cli.debug.ipython import ipython
 from kcl.sqlalchemy.clickapp.cli.debug.database.database import database
 from kcl.sqlalchemy.clickapp.cli.debug.config import config
 from kcl.sqlalchemy.clickapp.cli.debug._create._create import _create
-from kcl.sqlalchemy.clickapp.cli.debug._list._list import _list
-from kcl.sqlalchemy.clickapp.cli.debug._count._count import _count
+
 
 @click.group()
 def debug():
     pass
 
+
 debug.add_command(ipython)
 debug.add_command(database)
 debug.add_command(config)
 debug.add_command(_create, name="create")
-debug.add_command(_list, name="list")
-debug.add_command(_count, name="count")
