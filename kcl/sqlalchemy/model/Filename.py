a3e1f45764e993c468f7102ceeb3e10155969f23
jakeogh <github.com@v6y.net>
2018-01-09 01:50:14 -0700

    auto-commit

--- kcl/sqlalchemy/model/Filename.py
+++ kcl/sqlalchemy/model/Filename.py
@@ -63,7 +63,8 @@ class Filename(BASE):
     @hybrid_property
     def filename_lower(self):
         #return bytes(self.filename).lower() #nope, sqlalchemy cant translate because LOWER() is not defined for bytea
-        latin1_str = str(bytes(self.filename), encoding='Latin1').lower()
+        #latin1_str = str(bytes(self.filename), encoding='Latin1').lower()
+        latin1_str = str(bytes(self.filename), encoding='Latin1')
         print("latin1_str:", latin1_str)
         return latin1_str
 
