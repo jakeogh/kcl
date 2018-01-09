5991417fe7508ff569486f2655b63c6fbd43f6f3
jakeogh <github.com@v6y.net>
2018-01-09 05:27:40 -0700

    auto-commit

--- kcl/sqlalchemy/clickapp/cli/debug/_count/_count.py
+++ kcl/sqlalchemy/clickapp/cli/debug/_count/_count.py
@@ -14,5 +14,5 @@ CONTEXT_SETTINGS['allow_extra_args'] = True
 def _count(ctx, table_name):
     with self_contained_session(ctx.obj.database) as session:
         constructed_test = 'select COUNT(*) from %s;' % table_name
-        answer = session.execute(constructed_test)
+        answer = session.execute(constructed_test).one()
         print(answer)
