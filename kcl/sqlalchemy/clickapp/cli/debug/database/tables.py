9f105ae4b4fa48f75ffafe9faaf6b93519898743
jakeogh <github.com@v6y.net>
2018-01-09 04:28:35 -0700

    auto-commit

--- kcl/sqlalchemy/clickapp/cli/debug/database/tables.py
+++ kcl/sqlalchemy/clickapp/cli/debug/database/tables.py
@@ -5,9 +5,10 @@ from kcl.sqlalchemy.table_list import table_list as sqla_list_tables
 from kcl.sqlalchemy.self_contained_session import self_contained_session
 
 @click.command()
+@click.option('--table', default=None)
 @click.option('--contents', is_flag=True)
 @click.pass_obj
-def tables(config, contents):
+def tables(config, table, contents):
     with self_contained_session(config.database) as session:
-        sqla_list_tables(database=config.database, contents=contents)
+        sqla_list_tables(database=config.database, contents=contents, table=table)
 
