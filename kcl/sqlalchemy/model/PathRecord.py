5888c59f46597fa4e52427e52a45992b3acd188b
jakeogh <github.com@v6y.net>
2018-01-10 17:42:26 -0700

    auto-commit

--- kcl/sqlalchemy/model/PathRecord.py
+++ kcl/sqlalchemy/model/PathRecord.py
@@ -29,7 +29,7 @@ class PathRecord(BASE):
     Does not appear to be a big deal because it's not exp and most paths have 0 spaces
 
     '''
-    __table_args__ = (UniqueConstraint('path_id', 'filename_id', 'position', 'previous_position'),) #previous_position?
+    __table_args__ = (UniqueConstraint('path_id', 'filename_id'),)
     path_id = Column(Integer,
                      ForeignKey("path.id"),
                      unique=False,
