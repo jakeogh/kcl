777a392e4c1874df9faaf86bfec8a6344aad1a43
jakeogh <github.com@v6y.net>
2018-01-10 23:39:04 -0700

    auto-commit

--- kcl/sqlalchemy/model/Path.py
+++ kcl/sqlalchemy/model/Path.py
@@ -61,7 +61,7 @@ class Path(BASE):
     filename = relationship("Filename", backref='paths')
 
     #path = column_property(parent.filename + b'/' + filename)
-    path = column_property(b'/' + filename.filename)
+    path = column_property(b'/' + filename[0].filename)
 
 
     #def __init__(self, session, parent, path):
