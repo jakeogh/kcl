76247ec24fa1498a55f85677d034412d6301bb38
jakeogh <github.com@v6y.net>
2018-02-21 17:02:46 -0700

    auto-commit

--- kcl/printops.py
+++ kcl/printops.py
@@ -6,6 +6,7 @@ import time
 import inspect
 from pprint import pprint
 
+
 def ceprint(*args, **kwargs):
     '''Simple debugging replacement for print()'''
     caller = sys._getframe(1).f_code.co_name
@@ -14,17 +15,25 @@ def ceprint(*args, **kwargs):
     depth = len(stack)
     mod = str(inspect.getmodule(frm[0]))
     source_file = mod.split()[-1].split('>')[0].split("'")[1].split('/')[-1]
-    head = ' '.join([str(depth).zfill(3), str("%.5f" % time.time()), str(os.getpid()), source_file, caller+'()'])
+    head = ' '.join([
+            str(depth).zfill(3),
+            str("%.5f" % time.time()),
+            str(os.getpid()),
+            source_file,
+            caller+'()'
+            ])
     print('{0: <49}'.format(head), *args, file=sys.stderr, **kwargs)
+
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
+
 def epprint(*args, **kwargs):
     pprint(*args, stream=sys.stderr, **kwargs)
+
 
 def print_object_attrs(obj):
     eprint("type(obj):", type(obj))
     obj_attrs = vars(obj)
     eprint('\n'.join("%s: %s" % item for item in obj_attrs.items()))
-
