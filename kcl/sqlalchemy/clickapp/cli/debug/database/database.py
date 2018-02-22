b4d87a0a71b84d36f3c3b7be5cd98d68a1d0a11c
jakeogh <github.com@v6y.net>
2018-02-21 19:00:48 -0700

    auto-commit

--- kcl/sqlalchemy/clickapp/cli/debug/database/database.py
+++ kcl/sqlalchemy/clickapp/cli/debug/database/database.py
@@ -5,12 +5,14 @@ from kcl.sqlalchemy.clickapp.cli.debug.database._list._list import _list
 from kcl.sqlalchemy.clickapp.cli.debug.database._count._count import _count
 from kcl.sqlalchemy.clickapp.cli.debug.database._create._create import _create
 from kcl.sqlalchemy.clickapp.cli.debug.database._print import _print
+from kcl.sqlalchemy.clickapp.cli.debug.database._search import _search
 
 @click.group()
 def database():
     pass
 
 database.add_command(_print, name="print")
+database.add_command(_search, name="search")
 database.add_command(_list, name="list")
 database.add_command(_create, name="create")
 database.add_command(_count, name="count")
