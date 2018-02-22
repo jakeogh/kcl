7bc4f6de1932278ce970370de01d576131daef6f
jakeogh <github.com@v6y.net>
2018-02-21 17:20:46 -0700

    auto-commit

--- kcl/sqlalchemy/model/Path.py
+++ kcl/sqlalchemy/model/Path.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import create_engine
-#from sqlalchemy.orm import scoped_session
-#from sqlalchemy.orm import sessionmaker
-#from sqlalchemy.ext.declarative import declarative_base
-#from sqlalchemy.pool import NullPool
-#from sqlalchemy_utils.functions import create_database
-#from sqlalchemy_utils.functions import database_exists
 from sqlalchemy import Column, ForeignKey, Integer
 from sqlalchemy import func
 from sqlalchemy import CheckConstraint
@@ -23,6 +16,7 @@ from sqlalchemy.sql import select
 from kcl.printops import ceprint
 from kcl.sqlalchemy.model.BaseMixin import BASE
 from kcl.sqlalchemy.self_contained_session import self_contained_session
+from kcl.sqlalchemy.get_one_or_create import get_one_or_create
 
 
 def msg(msg, *args):
@@ -43,51 +37,6 @@ def msg(msg, *args):
     #print str(q.statement.compile(dialect=postgresql.dialect()))
 
 '''
-
-
-def get_one_or_create(session, model, *args, create_method='', create_method_kwargs=None, **kwargs):
-    '''
-        Find and return existing ORM object or create and return a new one. Adapted from examples.
-    '''
-    assert session
-    #print('')
-    #ceprint("entering get_one_or_create() model:", model, kwargs)
-    #for key in kwargs.keys(): # not sure why this was necessary
-    #    if issubclass(kwargs[key].__class__, model):
-    #        ceprint("returning early with kwargs["+key+"]:", kwargs[key])
-    #        return kwargs[key]
-    #    ceprint("key:", key)
-    #    ceprint("kwargs[key]:", kwargs[key])
-    try:
-        #ceprint("session.query filter_by:", kwargs)
-        result = session.query(model).filter_by(**kwargs).one()
-    except NoResultFound as e:
-        #ceprint("NoResultFound")
-        #import pdb; pdb.set_trace()
-        kwargs.update(create_method_kwargs or {})
-        created = getattr(model, create_method, model)(*args, **kwargs)
-        try:
-            session.add(created)
-            session.flush(objects=[created])
-            #ceprint("created.id:", created.id, "type(created):", type(created), "created:", created)
-            return created
-        except IntegrityError as e:
-            #ceprint("IntegrityError:", e, model)
-            #ceprint("calling session.rollback()")
-            session.rollback() # inklesspen | get_one_or_create() is _sometimes_ going to roll back a transaction
-
-            # catches the race condition assuming the IntegrityError was due to the race hitting a unique constraint
-            # in the case where the IntegrityError was caused by something other than a race, like voilating a
-            # CheckConstraint("position(' ' in word) = 0") then result will be None
-            # if so, it makes sense to re-raise the IntegrityError so the calling code can do something about it.
-            try:
-                result = session.query(model).filter_by(**kwargs).one()
-            except NoResultFound:
-                #ceprint("re-raising IntegrityError")
-                #import pdb; pdb.set_trace()
-                raise e
-            return result
-    return result
 
 
 class Filename(BASE):
