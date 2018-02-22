3a85c73fea8aa252d6bbe66e27d09fe6e92e2726
jakeogh <github.com@v6y.net>
2018-02-21 17:06:23 -0700

    auto-commit

--- kcl/sqlalchemy/clickapp/clickapp.py
+++ kcl/sqlalchemy/clickapp/clickapp.py
@@ -9,10 +9,11 @@ from sqlalchemy_utils.functions import create_database
 from sqlalchemy_utils.functions import drop_database
 from kcl.printops import eprint
 from kcl.sqlalchemy.self_contained_session import self_contained_session
-from kcl.sqlalchemy.BaseMixin import BASE
+from kcl.sqlalchemy.model.BaseMixin import BASE
 from kcl.click.CONTEXT_SETTINGS import CONTEXT_SETTINGS
 
 __version__ = 0.01
+
 
 @click.group(context_settings=CONTEXT_SETTINGS, help="boilerplate orm interface")
 @click.option('--verbose', is_flag=True)
@@ -44,4 +45,3 @@ def clickapp(ctx, verbose, database, temp_database, delete_database, debug):
     ctx.obj = CONFIG
     if debug:
         from pudb import set_trace; set_trace(paused=True)
-
