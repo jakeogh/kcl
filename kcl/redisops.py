3532445024cd11a6ba681cbfd894e42d7ba410ab
jakeogh <github.com@v6y.net>
2018-08-29 00:30:33 -0700

    auto-commit

--- kcl/redisops.py
+++ kcl/redisops.py
@@ -6,4 +6,4 @@ from .printops import ceprint
 r = redis.StrictRedis(host='127.0.0.1')
 
 def redis_add_iri_to_visited_ordered_set(key, iri, timestamp):
-    r.zadd(name=key, timestamp, iri)
+    r.zadd(timestamp, iri, name=key)
