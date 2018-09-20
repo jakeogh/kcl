7437624f0cb88a4074061a55df44cd957576c4f1
jakeogh <github.com@v6y.net>
2018-09-20 12:35:37 -0700

    auto-commit

--- kcl/cli/_htmlops/_htmlops.py
+++ kcl/cli/_htmlops/_htmlops.py
@@ -2,7 +2,7 @@
 
 import click
 from kcl.cli._htmlops._lxml import _lxml
-#from kcl.cli._htmlops._text import _text
+from kcl.cli._htmlops._text import _text
 
 
 @click.group()
@@ -11,4 +11,4 @@ def _htmlops():
 
 
 _htmlops.add_command(_lxml, name='lxml')
-#_htmlops.add_command(_text, name='text')
+_htmlops.add_command(_text, name='text')
