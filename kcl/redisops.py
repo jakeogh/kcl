70a8a3ab63b680042926d17798eecb3ba754222f
jakeogh <github.com@v6y.net>
2019-01-06 23:38:16 -0700

    auto-commit

--- kcl/redisops.py
+++ kcl/redisops.py
@@ -7,8 +7,10 @@ r = redis.StrictRedis(host='127.0.0.1')
 
 
 def add_to_ordered_set(key, value, timestamp):
-    r.zadd(key, timestamp, value)
+    return r.zadd(key, timestamp, value)
 
+def get_ordered_set(key, start=0, stop=-1, withscores=False)
+    return r.zrange(key, start, stop, withscores)
 
 def check_for_value_in_set(key, value, timestamp=False):
     #ceprint("key:", key, "value:", value)
