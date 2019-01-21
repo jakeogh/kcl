c9feeed53572588b2dbac76abb4a94680a405d13
jakeogh <github.com@v6y.net>
2019-01-21 15:30:43 -0700

    auto-commit

--- kcl/dirops.py
+++ kcl/dirops.py
@@ -18,7 +18,7 @@ def all_files_iter(p):
     if isinstance(p, str) or isinstance(p, bytes):
         p = Path(p)
     assert isinstance(p, Path)
-    print("yeilding p.absolute():", p.absolute())
+    #print("yeilding p.absolute():", p.absolute())
     yield bytes(p.absolute())
     for sub in p.iterdir():
         # eprint("sub:", sub)  # todo: read by terminal, so bell etc happens.... eprint bug?
