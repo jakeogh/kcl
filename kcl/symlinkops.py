4cd4809db9426c6da5b85334278f20dc10d1edec
jakeogh <github.com@v6y.net>
2019-01-20 17:35:08 -0700

    auto-commit

--- kcl/symlinkops.py
+++ kcl/symlinkops.py
@@ -13,24 +13,29 @@ from kcl.fileops import path_exists
 import os
 import subprocess
 
+
 def is_symlink(infile):
     if os.path.islink(infile):
         return True
     return False
 
+
 def is_symlink_to_dir(link):
     if os.path.islink(link):
         return os.path.isdir(link)
+
 
 def is_broken_symlink(path):
     if os.path.islink(path): # path is a symlink
         return not os.path.exists(path) # returns False for broken symlinks
     return False # path isnt a symlink
 
+
 def is_unbroken_symlink(path):
     if os.path.islink(path): # path is a symlink
         return os.path.exists(path) # returns False for broken symlinks
     return False # path isnt a symlink
+
 
 def get_symlink_abs_target(link): # assumes link is unbroken
     target = os.readlink(link)
@@ -38,12 +43,14 @@ def get_symlink_abs_target(link): # assumes link is unbroken
     target_file = os.path.realpath(target_joined)
     return target_file
 
+
 def is_unbroken_symlink_to_target(target, link):    #bug, should not assume unicode paths
     if is_unbroken_symlink(link):
         link_target = get_symlink_abs_target(link)
         if link_target == target:
             return True
     return False
+
 
 def create_relative_symlink(target, link_name):
     target = os.path.abspath(target)
@@ -54,7 +61,7 @@ def create_relative_symlink(target, link_name):
 
     if is_broken_symlink(link_name):
         ceprint('ERROR: %s exists as a broken symlink. ' +
-            'Remove it before trying to make a new symlink. Exiting.' % link_name)
+            'Remove it before trying to make a new symlink. Exiting.', link_name)
         quit(1)
 
     link_name_folder = '/'.join(link_name.split('/')[:-1])
@@ -64,6 +71,7 @@ def create_relative_symlink(target, link_name):
 
     relative_target = os.path.relpath(target, link_name_folder)
     os.symlink(relative_target, link_name)
+
 
 def symlink_destination(link): #broken for multi level symlinks
     #ceprint(link)
@@ -81,6 +89,7 @@ def symlink_destination(link): #broken for multi level symlinks
     dest = os.path.realpath(p)
     return dest
 
+
 def readlinkf(path): # ugly
         p = subprocess.Popen(['readlink', '-f', path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         readlink_output, errors = p.communicate()
@@ -90,10 +99,12 @@ def readlinkf(path): # ugly
         else:
             return readlink_output_clean
 
+
 def get_symlink_target_next(path):
         assert os.path.islink(path)
         target = os.readlink(path)
         return target
+
 
 def get_symlink_target_final(path): #broken for bytes
         if os.path.islink(path):
@@ -105,6 +116,7 @@ def get_symlink_target_final(path): #broken for bytes
         #print("target_file:", target_file)
         return target_file
 
+
 def symlink_or_exit(target, link_name):
     try:
         os.symlink(target, link_name)
@@ -112,6 +124,7 @@ def symlink_or_exit(target, link_name):
         print("Got Exception: %s", e)
         print("Unable to symlink link_name: %s to target: %s Exiting." % (link_name, target))
         os._exit(1)
+
 
 #def symlink_or_exit(target, link_name, hash_folder=''):
 #    if hash_folder != '':
