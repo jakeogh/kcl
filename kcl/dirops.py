e5c9c5cfe841566694b0b962d1492f606d839af9
jakeogh <github.com@v6y.net>
2019-01-21 15:16:25 -0700

    auto-commit

--- kcl/dirops.py
+++ kcl/dirops.py
@@ -19,15 +19,15 @@ def all_files_iter(p):
         p = Path(p)
     assert isinstance(p, Path)
     print("yeilding p:", p)
-    yield p.abspath()
+    yield p.absolute()
     for sub in p.iterdir():
         # eprint("sub:", sub)  # todo: read by terminal, so bell etc happens.... eprint bug?
         if sub.is_symlink():  # must be before is_dir()
-            yield sub.abspath()
+            yield sub.absolute()
         elif sub.is_dir():
             yield from all_files_iter(sub)
         else:
-            yield sub.abspath()
+            yield sub.absolute()
 
 
 def all_files(folder, files_only=False): # todo add flags for recursive, follow symlinks etc, return a generator
