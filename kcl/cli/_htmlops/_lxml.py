3290ca68ea536ab255fe74109e775b1e63440dec
jakeogh <github.com@v6y.net>
2019-01-29 02:59:00 -0700

    auto-commit

--- kcl/cli/_htmlops/_lxml.py
+++ kcl/cli/_htmlops/_lxml.py
@@ -7,12 +7,16 @@ from kcl.printops import ceprint
 @click.command()
 @click.argument('path', type=click.Path(exists=True, dir_okay=False, path_type=bytes, allow_dash=False, resolve_path=False), nargs=1, required=True)
 @click.argument('iri', type=str, nargs=1, required=True)
+@click.option('--link-text', is_flag=True)
 @click.option('--verbose', is_flag=True)
 @click.pass_obj
-def _lxml(config, path, iri, verbose):
+def _lxml(config, path, iri, link_text, verbose):
     if verbose:
         ceprint("path:", path)
         ceprint("iri:", iri)
-    urls = extract_urls_lxml(path, iri)
+    urls = extract_urls_lxml(html_file=path, uri=iri, link_text=link_text)
     for url in urls:
-        print(url)
+        if link_text:
+            print(url[0], url[1])
+        else:
+            print(url)
