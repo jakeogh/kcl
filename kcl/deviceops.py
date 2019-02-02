0863ac73a8957854ff893987b0f70f3dd2cc7f9d
jakeogh <github.com@v6y.net>
2019-02-02 16:40:41 -0700

    auto-commit

--- kcl/deviceops.py
+++ kcl/deviceops.py
@@ -102,3 +102,22 @@ def destroy_block_device_head_and_tail(device, size, note, force, no_backup):
     destroy_block_device_tail(device=device, size=size, note=note, no_backup=no_backup)
 
 
+@click.command()
+@click.argument('devices', required=True, nargs=-1)
+@click.option('--size', is_flag=False, required=False, type=int, default=(1024*1024*128))
+@click.option('--note', is_flag=False, required=False)
+@click.option('--force', is_flag=True, required=False)
+@click.option('--no-backup', is_flag=True, required=False)
+def destroy_block_devices_head_and_tail(devices, size, note, force, no_backup):
+    for device in devices:
+        assert isinstance(no_backup, bool)
+        assert not device[-1].isdigit()
+        eprint("destroying device:", device)
+        assert path_is_block_special(device)
+        assert not block_special_path_is_mounted(device)
+
+    if not force:
+        warn(devices)
+
+    for device in devices:
+        destroy_block_device_head_and_tail(device, size, note, force, no_backup)
