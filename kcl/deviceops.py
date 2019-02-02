b3255629e932e2a9d41531807839216de89ca8d4
jakeogh <github.com@v6y.net>
2019-02-02 03:16:43 -0700

    auto-commit

---
+++ kcl/deviceops.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import time
+from kcl.mountops import block_special_path_is_mounted
+from kcl.fileops import path_is_block_special
+from kcl.printops import eprint
+
+
+def warn(devices):
+    assert isinstance(devices, tuple)
+    for device in devices:
+        assert path_is_block_special(device)
+        assert not block_special_path_is_mounted(device)
+    eprint("THIS WILL DESTROY ALL DATA ON", ' '.join(devices), "_REMOVE_ ANY HARD DRIVES (and removable storage like USB sticks) WHICH YOU DO NOT WANT TO ACCIDENTLY DELETE THE DATA ON")
+    answer = input("Do you want to proceed with deleting all of your data? (type YES to proceed)")
+    if answer != 'YES':
+        quit(1)
+    eprint("Sleeping 5 seconds")
+    time.sleep(5)
