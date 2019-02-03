e888cc08077362458ff176228821bb8f9fe25646
jakeogh <github.com@v6y.net>
2019-02-02 17:13:10 -0700

    auto-commit

--- kcl/filesystemops.py
+++ kcl/filesystemops.py
@@ -4,16 +4,15 @@ import click
 from kcl.fileops import path_is_block_special
 from kcl.mountops import block_special_path_is_mounted
 from kcl.command import run_command
-from kcl.printops import ceprint
+from kcl.printops import eprint
 from kcl.deviceops import warn
-
 
 @click.command()
 @click.option('--device', is_flag=False, required=True)
 @click.option('--filesystem', "filesystem", is_flag=False, required=True, type=click.Choice(['fat16', 'fat32']))
 @click.option('--force', is_flag=True, required=False)
 def create_filesystem(device, filesystem, force):
-    ceprint("creating", filesystem, "filesystem on:", device)
+    eprint("creating", filesystem, "filesystem on:", device)
     assert device[-1].isdigit()
     # oddly, this failed on '/dev/sda2', maybe the kernel was not done
     # digesting the previous table change? (using fat16)
