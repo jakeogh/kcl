ab1d0b845476f66938d995f581bfaef36baa9f6f
jakeogh <github.com@v6y.net>
2019-02-23 19:57:03 -0700

    auto-commit

--- kcl/htmlops.py
+++ kcl/htmlops.py
@@ -39,8 +39,9 @@ def extract_urls_lxml(html_file, url, verbose=False):
     if verbose: ceprint("len(html):", len(html))
     url_list = []
     try:
-        #dom = lxml.html.fromstring(html, base_url=url, parser=parser)
-        dom = lxml.html.fromstring(html)
+        dom = lxml.html.fromstring(html, base_url=url, parser=parser)
+        dom = dom.getroot()
+        #dom = lxml.html.fromstring(html)
         #e_tree = etree.parse(html_file, base_url=url, parser=parser)
         #dom = lxml.html.parse(html_file, base_url=url, parser=parser)
     #except ValueError:
