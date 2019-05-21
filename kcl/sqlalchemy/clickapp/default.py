a9509259755428316849d9b3504668708e912484
jakeogh <github.com@v6y.net>
2019-05-21 01:53:02 -0700

    auto-commit

--- kcl/sqlalchemy/clickapp/default.py
+++ kcl/sqlalchemy/clickapp/default.py
@@ -3,8 +3,8 @@
 import builtins
 CONFIG = builtins.config
 
-from kcl.printops import eprint
-from kcl.printops import ceprint
+#from kcl.printops import eprint
+#from kcl.printops import ceprint
 
 
 from kcl.sqlalchemy.clickapp.cli.debug.debug import debug
@@ -15,4 +15,3 @@ CONFIG.appobject.add_command(debug)
 
 from kcl.sqlalchemy.clickapp.test import test
 CONFIG.appobject.add_command(test)
-ceprint("here")
