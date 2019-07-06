6d2015134b5b07c6cb446cf436b99d542eefb16e
jakeogh <github.com@v6y.net>
2019-07-05 23:52:42 -0700

    auto-commit

--- kcl/sqlalchemy/delete_and_recreate_database.py
+++ kcl/sqlalchemy/delete_and_recreate_database.py
@@ -3,15 +3,15 @@
 
 from kcl.printops import eprint
 from kcl.sqlalchemy.delete_database import delete_database
-from kcl.sqlalchemy.create_database import create_database
 from sqlalchemy_utils.functions import create_database
 from sqlalchemy.exc import ProgrammingError
+
 
 def delete_and_recreate_database(database):
     try:
         delete_database(database=database)
     except ProgrammingError:
-            pass #db didnt exist
+            pass  # db didnt exist
     finally:
         create_database(database=database)
 
