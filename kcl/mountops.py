18158ca54b05c19d4e6dd75cdd02d7b006e0f46a
jakeogh <github.com@v6y.net>
2019-08-04 21:09:43 -0700

    better

--- kcl/mountops.py
+++ kcl/mountops.py
@@ -10,25 +10,29 @@
 # TODO: https://docs.python.org/3.7/library/pathlib.html#pathlib.Path.is_mount
 
 import os
+from pathlib import Path
 from .fileops import path_is_block_special
 from psutil import disk_partitions
 from .printops import ceprint
 
 def block_special_path_is_mounted(path):
+    assert path
+    path = Path(os.fsencode(path))
+    assert isinstance(path, Path)
     for mount in disk_partitions():
         #print(mount)
-        if path in mount.device:
+        if path.as_posix() in mount.device:
             return True
     return False
 
 
 def path_is_mounted(path, verbose=False):  # todo test with angryfiles
-    path = path.strip()
     assert path
-    assert isinstance(path, str)  # fixme
+    path = Path(os.fsencode(path))
+    assert isinstance(path, Path)
     for mount in disk_partitions():
         if verbose: ceprint("mount:", mount)
-        if mount.mountpoint == path:
+        if mount.mountpoint == path.as_posix():
             return True
     if os.path.ismount(path):
         return True
