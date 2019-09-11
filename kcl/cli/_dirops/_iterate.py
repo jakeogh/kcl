68191eb773afc3c43519a307d9731ed24b997ad7
jakeogh <github.com@v6y.net>
2019-09-11 11:06:18 -0700

    auto-commit

--- kcl/cli/_dirops/_iterate.py
+++ kcl/cli/_dirops/_iterate.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 import click
-from kcl.dirops import Path_Iterator
+from getdents import paths
 from kcl.printops import ceprint
-from kcl.printops import eprint
-from getdents import DentGen
+#from kcl.printops import eprint
+#from getdents import DentGen
 
 @click.command()
 @click.argument('path', type=click.Path(exists=True, dir_okay=True, file_okay=False, allow_dash=False, resolve_path=False), nargs=1, required=True)
@@ -13,6 +13,8 @@ from getdents import DentGen
 def _iterate(config, path, verbose):
     if verbose:
         ceprint("path:", path)
-    dentgen = DentGen(path).go()
-    for item in dentgen:
+    for item in paths(path):
         print(item)
+    #dentgen = DentGen(path).go()
+    #for item in dentgen:
+    #    print(item)
