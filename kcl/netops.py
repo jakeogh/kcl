15d22b249b73a0cc9c3f4d6cab47295b4eb9ae84
jakeogh <github.com@v6y.net>
2019-10-05 12:32:49 -0700

    auto-commit

--- kcl/netops.py
+++ kcl/netops.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 
 import requests
+from kcl.assertops import verify
 from kcl.printops import eprint
 
 
-def download_file(url, destination_dir, force=False):
+def download_file(url, destination_dir, force=False, proxy=None):
     eprint("downloading:", url)
     local_filename = destination_dir + '/' + url.split('/')[-1]
     #if force:
     #    os.unlink(local_filename)
-    r = requests.get(url, stream=True)
+    proxy_dict = {}
+    if proxy:
+        verify(proxy.startswith('http'))
+        verify(len(proxy.split(":")) == 3)
+        proxy_dict["http"] = proxy
+        proxy_dict["https"] = proxy
+
+    r = requests.get(url, stream=True, proxies=proxy_dict)
     try:
         with open(local_filename, 'bx') as fh:
             for chunk in r.iter_content(chunk_size=1024*1024):
