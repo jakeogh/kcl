6b32e407debad7fc2914540311e6f40710e10aac
jakeogh <github.com@v6y.net>
2020-04-24 18:51:40 -0700

    auto-commit

--- kcl/byteops.py
+++ kcl/byteops.py
@@ -7,9 +7,9 @@
 #
 # common functions acting on bytes
 
-import requests
-from kcl.logops import leprint
-from kcl.logops import LOG
+#import requests
+#from kcl.logops import leprint
+#from kcl.logops import LOG
 
 
 def get_random_bytes(count, exclude=[]):
@@ -21,7 +21,7 @@ def get_random_bytes(count, exclude=[]):
             new_bytes = bytearray(python_fd.read(bytes_needed))
             for exclude_byte in exclude:
                 new_bytes = new_bytes.replace(exclude_byte, b'')
-            print("len(new_bytes):", len(new_bytes))
+            #print("len(new_bytes):", len(new_bytes))
             accepted_bytes = accepted_bytes + new_bytes
         return accepted_bytes
 
@@ -50,30 +50,30 @@ def remove_comments_from_bytes(line): #todo check for (assert <=1 line break) mu
     return uncommented_line
 
 
-def read_url_bytes(url):
-    leprint("GET: %s", url, level=LOG['DEBUG'])
-    user_agent = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:24.0) Gecko/20100101 Firefox/24.0'
-    try:
-        raw_url_bytes = requests.get(url, headers={'User-Agent': user_agent},
-            allow_redirects=True, stream=False, timeout=15.500).content
-    except Exception as e:
-        leprint(e, level=LOG['WARNING'])
-        return False
-    return raw_url_bytes
-
-
-def read_url_bytes_and_cache(url, cache=True):
-    raw_url_bytes = read_url_bytes(url)
-    if cache:
-        cache_index_file = CACHE_DIRECTORY + '/sha1_index'
-        cache_file = generate_cache_file_name(url)
-        with open(cache_file, 'xb') as fh:
-            fh.write(raw_url_bytes)
-        line_to_write = cache_file + ' ' + url + '\n'
-        write_unique_line(line_to_write, cache_index_file)
-
-    if raw_url_bytes:
-        leprint("Returning %d bytes from %s", len(raw_url_bytes), url, level=LOG['DEBUG'])
-        return raw_url_bytes
-    else:
-        return False
+#def read_url_bytes(url):
+#    leprint("GET: %s", url, level=LOG['DEBUG'])
+#    user_agent = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:24.0) Gecko/20100101 Firefox/24.0'
+#    try:
+#        raw_url_bytes = requests.get(url, headers={'User-Agent': user_agent},
+#            allow_redirects=True, stream=False, timeout=15.500).content
+#    except Exception as e:
+#        leprint(e, level=LOG['WARNING'])
+#        return False
+#    return raw_url_bytes
+#
+#
+#def read_url_bytes_and_cache(url, cache=True):
+#    raw_url_bytes = read_url_bytes(url)
+#    if cache:
+#        cache_index_file = CACHE_DIRECTORY + '/sha1_index'
+#        cache_file = generate_cache_file_name(url)
+#        with open(cache_file, 'xb') as fh:
+#            fh.write(raw_url_bytes)
+#        line_to_write = cache_file + ' ' + url + '\n'
+#        write_unique_line(line_to_write, cache_index_file)
+#
+#    if raw_url_bytes:
+#        leprint("Returning %d bytes from %s", len(raw_url_bytes), url, level=LOG['DEBUG'])
+#        return raw_url_bytes
+#    else:
+#        return False
