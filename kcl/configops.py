591dffe23bafb9d50da76684cb872c738c30858c
jakeogh <github.com@v6y.net>
2020-06-25 15:32:29 -0700

    auto-commit

--- kcl/configops.py
+++ kcl/configops.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 from pathlib import Path
 import configparser
 from kcl.fileops import empty_file
@@ -38,18 +39,4 @@ def click_write_config_entry(click_instance, app_name, section, key, value):
 
     with open(cfg, 'w') as configfile:
         parser.write(configfile)
-
-
-def read_config_file(config_file):
-    # race condition, CONFIG.read throws no error when it trys to read empty or non existing config file
-    if os.path.isfile(config_file) and not empty_file(config_file):
-        config_parser = configparser.ConfigParser()
-        config_parser.read(config_file)
-        return config_parser
-    else:
-        print("Problem reading config file, creating default CONFIG.")
-        #write_default_config_file()
-        #config_parser.read(config_file)
-        #return config_parser
-
 
--- kcl/debugops.py
+++ kcl/debugops.py
@@ -6,3 +6,14 @@ import ipdb
 
 def embed_ipython():
     ipdb.set_trace()
+
+
+def pause(message, ipython=False):
+    assert isinstance(message, str)
+    if ipython:
+        message += " type 'ipython' to enter shell"
+    response = input(message)
+    if response == "ipython":
+        embed_ipython()
+        pause("press enter to continue execution")
+
