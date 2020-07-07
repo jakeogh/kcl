27b70da4497a0f0d0d138ade0702aba5c185e119
jakeogh <github.com@v6y.net>
2020-07-06 21:48:58 -0700

    auto-commit

--- kcl/inputops.py
+++ kcl/inputops.py
@@ -2,8 +2,10 @@
 
 import dmenu
 import pint
+import sys
 from icecream import ic
 from .printops import ceprint
+from .byteops import read_by_byte
 
 
 def passphrase_prompt(note):
@@ -50,24 +52,15 @@ def human_filesize_to_int(size, verbose=False):
     return result
 
 
-@click.option('--verbose', is_flag=True)
-@click.option('--debug', is_flag=True)
-@click.option('--head', type=int)
-@click.option("--null", is_flag=True)
-def cli(strings, add, group, verbose, debug, head, null):
+def input_iterator(null=False,
+                   strings=None,
+                   verbose=False,
+                   debug=False,
+                   output_limit=None):
 
     byte = b'\n'
     if null:
         byte = b'\x00'
-
-    if not group:
-        group = "BANS"
-
-    config, config_mtime = click_read_config(click_instance=click,
-                                             app_name=APP_NAME,
-                                             verbose=verbose)
-    if verbose:
-        ic(config)
 
     if strings:
         iterator = strings
@@ -82,31 +75,10 @@ def cli(strings, add, group, verbose, debug, head, null):
         if isinstance(string, bytes):
             string = string.decode('utf8')
 
-        if add:
-            section = "BANS"
-            key = string
-            value = ""
-            config, config_mtime = click_write_config_entry(click_instance=click,
-                                                            app_name=APP_NAME,
-                                                            section=section,
-                                                            key=key,
-                                                            value=value,
-                                                            verbose=verbose)
-        else:
-            banned = stringfilter(string=string,
-                                  group=None,
-                                  config=config,
-                                  config_mtime=config_mtime,
-                                  verbose=verbose,
-                                  debug=debug)
-            if banned:
-                print("banned:", string, file=sys.stderr)
-                continue
-            else:
-                print(string, end=byte.decode('utf8'))
-                lines_output += 1
+        yield string
+        lines_output += 1
 
-                if head:
-                    if lines_output >= head:
-                        break
+        if output_limit:
+            if lines_output >= output_limit:
+                return
 
