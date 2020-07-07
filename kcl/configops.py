e3248a82f704a84229603564f731d049a99c21cf
jakeogh <github.com@v6y.net>
2020-07-06 18:45:53 -0700

    auto-commit

--- kcl/configops.py
+++ kcl/configops.py
@@ -13,7 +13,7 @@ class ConfigUnchangedError(ValueError):
     pass
 
 
-def click_read_config(click_instance, app_name, verbose=False, last_mtime=None):
+def click_read_config(*, click_instance, app_name, verbose=False, last_mtime=None):
     cfg = Path(os.path.join(click_instance.get_app_dir(app_name), 'config.ini'))
     config_mtime = get_mtime(cfg)
     if config_mtime == last_mtime:
@@ -33,10 +33,11 @@ def click_read_config(click_instance, app_name, verbose=False, last_mtime=None):
             rv[section][key] = value
     if verbose:
         ic(rv)
+
     return rv, config_mtime
 
 
-def click_write_config_entry(click_instance, app_name, section, key, value, verbose=False):
+def click_write_config_entry(*, click_instance, app_name, section, key, value, verbose=False):
     cfg = Path(os.path.join(click_instance.get_app_dir(app_name), 'config.ini'))
     parser = configparser.RawConfigParser()
     parser.read([cfg])
