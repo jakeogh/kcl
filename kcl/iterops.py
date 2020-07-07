5a9feaabe5cf1e0815b89c755d6be3559dccc27e
jakeogh <github.com@v6y.net>
2020-07-07 12:51:42 -0700

    auto-commit

--- kcl/iterops.py
+++ kcl/iterops.py
@@ -2,6 +2,7 @@
 
 import time
 import secrets
+from kcl.printops import eprint
 from itertools import zip_longest
 from icecream import ic
 
@@ -15,28 +16,49 @@ def compact(items):
     return [item for item in items if item]
 
 
-# todo add memory limit
-def randomize_iterator(iterator, max_wait=0.01, verbose=False):
-    buffer_set = set()
-    buffer_set.add(next(iterator))
-
+def append_to_set_for_time(*, iterator, the_set, max_wait, verbose=False, debug=False):
     start_time = time.time()
-    if verbose:
-        ic(start_time)
-    while ((time.time() - start_time) > max_wait):
+
+    loops = 0
+    while (time.time() - start_time) < max_wait:
+        loops += 1
+        try:
+            the_set.add(next(iterator))
+        except StopIteration:
+            pass
+
+    assert loops > 0
+
+    return the_set
+
+
+# add time-like memory limit
+# the longer the max_wait, the larger buffer_set will be,
+# resulting in better mixing
+def randomize_iterator(iterator, buffer_set=None, max_wait=0.1, verbose=False, debug=False):
+
+    if not buffer_set:
+        buffer_set = set()
+        buffer_set.add(next(iterator))
+
+    buffer_set = append_to_set_for_time(iterator=iterator,
+                                        the_set=buffer_set,
+                                        max_wait=max_wait,
+                                        verbose=verbose,
+                                        debug=debug)
+
+    while buffer_set:
         try:
             buffer_set.add(next(iterator))
-        except StopIteration as e:
-            if verbose:
-                ic(e)
-            break
+        except StopIteration:
+            pass
 
-    random_index = secrets.randbelow(len(buffer_set))
-    next_item = list(buffer_set).pop(random_index)
-    buffer_set.remove(next_item)
+        random_index = secrets.randbelow(len(buffer_set))
+        #[next_item] = itertools.islice(buffer_set, random_index, 1)
+        next_item = list(buffer_set).pop(random_index)
+        buffer_set.remove(next_item)
 
-    if verbose:
-        ic(len(buffer_set))
+        if verbose:
+            eprint("len(buffer_set):", len(buffer_set))
 
-    yield next_item
-
+        yield next_item
