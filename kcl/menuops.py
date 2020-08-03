2054521b4e6a7843218d410b1b57f681659773b9
jakeogh <github.com@v6y.net>
2020-08-02 23:48:04 -0700

    auto-commit

--- kcl/menuops.py
+++ kcl/menuops.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python3
+
+
 import os
 from icecream import ic
 from .commandops import run_command
 
 
-def prompt_tag_dmenu(tag_cache_file="/home/user/.iridb/.dmenu_tag_cache", verbose=False):
-    command = '''dmenu -fn "-misc-fixed-*-*-*-*-20-*-*-*-*-*-*-*" -f -nb "#000000" -i <''' + tag_cache_file
+def prompt_tag_dmenu(cache_file="/home/user/.iridb/.dmenu_tag_cache", verbose=False):
+    command = '''dmenu -fn "-misc-fixed-*-*-*-*-20-*-*-*-*-*-*-*" -f -nb "#000000" -i <''' + cache_file
     #text = os.popen(command).read()
     text = run_command(command, popen=True, verbose=verbose, str_output=True)
     text = text.strip()
@@ -13,11 +16,16 @@ def prompt_tag_dmenu(tag_cache_file="/home/user/.iridb/.dmenu_tag_cache", verbos
     return text
 
 
-def prompt_tag_slmenu(file="/home/user/.iridb/.dmenu_tag_cache"):
-    command = "/usr/bin/slmenu -i <" + file
+def prompt_tag_slmenu(cache_file="/home/user/.iridb/.dmenu_tag_cache"):
+    command = "/usr/bin/slmenu -i <" + cache_file
     text = os.popen(command).read()
-
     #tag_completer = WordCompleter(tags, ignore_case=True)
     #text = prompt('Tag: ', completer=tag_completer,
     #              complete_style=CompleteStyle.READLINE_LIKE, complete_while_typing=True)
     return text
+
+
+def prompt_tag(cache_file, verbose=False):
+    tag = prompt_tag_dmenu(cache_file=cache_file, verbose=verbose)
+    if verbose:
+        ic(tag)
