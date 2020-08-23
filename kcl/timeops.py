2b5b52afd1e370dc545e74dfa438677cab1545e3
jakeogh <github.com@v6y.net>
2020-08-23 11:25:47 -0700

    auto-commit

--- kcl/timeops.py
+++ kcl/timeops.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
+
+# pylint: disable=C0111     # docstrings are always outdated and wrong
+# pylint: disable=W0511     # todo is encouraged
+# pylint: disable=C0301     # line too long
+# pylint: disable=R0902     # too many instance attributes
+# pylint: disable=C0302     # too many lines in module
+# pylint: disable=C0103     # single letter var names, func name too descriptive
+# pylint: disable=R0911     # too many return statements
+# pylint: disable=R0912     # too many branches
+# pylint: disable=R0915     # too many statements
+# pylint: disable=R0913     # too many arguments
+# pylint: disable=R1702     # too many nested blocks
+# pylint: disable=R0914     # too many local variables
+# pylint: disable=R0903     # too few public methods
+# pylint: disable=E1101     # no member for base
+# pylint: disable=W0201     # attribute defined outside __init__
+
+
 import os
 import time
 import errno
@@ -9,23 +27,29 @@ from .printops import eprint
 from icecream import ic
 
 
-def delaygen(start, multiplier, end):
-    start = float(start)
-    multiplier = float(multiplier)
-    end = float(end)
-    assert start >= 0
-    assert end > 0
-    assert multiplier > 0
-    assert start <= end
-    delay = start
-    yield delay
-    delay = delay + (delay * multiplier)
-    while delay < end:
-        yield delay
-        delay = delay + (delay * multiplier)
-    delay = end
-    while True:
-        yield delay
+class Delay():
+    def __init__(self, start, multiplier, end):
+        start = float(start)
+        multiplier = float(multiplier)
+        end = float(end)
+        assert start >= 0
+        assert end > 0
+        assert multiplier > 0
+        assert start <= end
+        delay = start
+        self.delay = delay
+        self.multiplier = multiplier
+        self.end = end
+
+    def sleep(self):
+        time.sleep(self.delay)
+        self.delay = self.delay + (self.delay * self.multiplier)
+        while self.delay < self.end:
+            time.sleep(self.delay)
+            self.delay = self.delay + (self.delay * self.multiplier)
+        self.delay = self.end
+        while True:
+            time.sleep(self.delay)
 
 
 def timestamp():
