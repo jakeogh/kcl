ba72b1b8358d8a4d5a3ddd1eb588343e7bfff102
jakeogh <github.com@v6y.net>
2020-08-26 22:18:42 -0700

    auto-commit

--- kcl/cli/_htmlops/_text.py
+++ kcl/cli/_htmlops/_text.py
@@ -1,16 +1,40 @@
 #!/usr/bin/env python3
 
 import click
+from icecream import ic
 from kcl.htmlops import extract_iris_from_text_file
+from kcl.htmlops import extract_iris_from_text
+from kcl.inputops import enumerate_input
 from kcl.printops import ceprint
 
+
 @click.command()
-@click.argument('path', type=click.Path(exists=True, dir_okay=False, path_type=bytes, allow_dash=False, resolve_path=False), nargs=1, required=True)
+@click.argument('paths', type=str, nargs=-1, required=True)
 @click.option('--verbose', is_flag=True)
+@click.option('--debug', is_flag=True)
+@click.option('--notpaths', is_flag=True)
+@click.option('--null', is_flag=True)
 @click.pass_obj
-def _text(config, path, verbose):
-    if verbose:
-        ceprint("path:", path)
-    urls = extract_iris_from_text_file(path)
-    for url in urls:
-        print(url)
+def _text(config,
+          paths,
+          notpaths,
+          null,
+          verbose,
+          debug):
+
+    for index, path in enumerate_input(iterator=paths,
+                                       null=null,
+                                       debug=debug,
+                                       verbose=verbose):
+        if verbose:
+            ic(index, path)
+
+        if verbose:
+            ceprint("path:", path)
+        if notpaths:
+            urls = extract_iris_from_text(path)
+        else:
+            urls = extract_iris_from_text_file(path)
+
+        for url in urls:
+            print(url)
