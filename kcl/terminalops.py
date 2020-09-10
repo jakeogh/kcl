2a2373799008f7df6b6cfa9210a3c3bc1f0da144
jakeogh <github.com@v6y.net>
2020-09-09 17:36:55 -0700

    auto-commit

--- kcl/terminalops.py
+++ kcl/terminalops.py
@@ -9,6 +9,16 @@ def get_current_virtural_terminal():
     return int(output)
 
 
+def in_xorg():
+    vt = get_current_virtural_terminal()
+    output = run_command('w').decode('utf8').splitlines()
+    for line in output:
+        if '/usr/bin/X' in line:
+            if line.endswith('vt' + str(vt)):
+                return True
+    return False
+
+
 def get_terminal_width(default=80):
     width, height = shutil.get_terminal_size((default, 20))
     return width
