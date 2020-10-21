4199be673ed87f41f247ccd3caa347f7c5838a5d
jakeogh <github.com@v6y.net>
2020-10-20 22:19:39 -0700

    auto-commit

--- kcl/deviceops.py
+++ kcl/deviceops.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
 
+# pylint: disable=C0111  # docstrings are always outdated and wrong
+# pylint: disable=W0511  # todo is encouraged
+# pylint: disable=C0301  # line too long
+# pylint: disable=R0902  # too many instance attributes
+# pylint: disable=C0302  # too many lines in module
+# pylint: disable=C0103  # single letter var names, func name too descriptive
+# pylint: disable=R0911  # too many return statements
+# pylint: disable=R0912  # too many branches
+# pylint: disable=R0915  # too many statements
+# pylint: disable=R0913  # too many arguments
+# pylint: disable=R1702  # too many nested blocks
+# pylint: disable=R0914  # too many local variables
+# pylint: disable=R0903  # too few public methods
+# pylint: disable=E1101  # no member for base
+# pylint: disable=W0201  # attribute defined outside __init__
+# pylint: disable=R0916  # Too many boolean expressions in if statement
+
+
 import os
 import time
 import click
@@ -11,7 +29,6 @@ from kcl.fileops import get_block_device_size
 from kcl.fileops import path_exists
 from kcl.printops import eprint
 from kcl.commandops import run_command
-#from kcl.filesystemops import create_filesystem
 from kcl.warnops import warn
 from kcl.inputops import passphrase_prompt
 
@@ -33,12 +50,14 @@ def wait_for_block_special_device_to_exist(path, timeout=5):
             break
     return True
 
+
 def add_partition_number_to_device(device, partition_number, verbose=False):
     if Path(device).name.startswith('nvme'):
         devpath = device + 'p' + partition_number
     else:
         devpath = device + partition_number
     return devpath
+
 
 @deviceops.command()
 @click.argument('device', required=True, nargs=1)
