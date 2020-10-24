4237aebcd65a1a14a349be1211ea20fb1508158f
jakeogh <github.com@v6y.net>
2020-10-24 12:58:09 -0700

    auto-commit

--- kcl/exceptionops.py
+++ kcl/exceptionops.py
@@ -5,12 +5,12 @@ import time
 from icecream import ic
 
 
-def retry_on_exception(*, function, exception, kwargs={}, delay=1, delay_multiplier=0.5):
+def retry_on_exception(*, function, exceptions, kwargs={}, delay=1, delay_multiplier=0.5):
     while True:
         try:
             return function(**kwargs)
             break
-        except exception as e:
+        except exceptions as e:
             ic(e)
             #print(e, file=sys.stderr)
             delay = delay + (delay * delay_multiplier)
