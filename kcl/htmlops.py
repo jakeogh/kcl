a722dd4b347d6d7e7534bd3f038b44af0e8908a8
jakeogh <github.com@v6y.net>
2020-11-09 03:13:07 -0700

    auto-commit

--- kcl/htmlops.py
+++ kcl/htmlops.py
@@ -60,6 +60,7 @@ def extract_title_from_file(data_file, verbose=False):
 
 
 def parse_html_to_dom(html):
+    assert not instance(html, bytes)
     dom_tree = html.fromstring(html)
     return dom_tree
 
