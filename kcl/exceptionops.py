087bdba8a877a4b1b5b9f8d8232d2ac18edcd76b
jakeogh <github.com@v6y.net>
2020-11-14 00:59:59 -0700

    auto-commit

--- kcl/exceptionops.py
+++ kcl/exceptionops.py
@@ -4,7 +4,14 @@ import time
 from icecream import ic
 
 
-def retry_on_exception(*, function, exceptions, kwargs={}, delay=1, delay_multiplier=0.5):
+def retry_on_exception(*,
+                       function,
+                       exceptions,
+                       errno,
+                       kwargs={},
+                       delay=1,
+                       delay_multiplier=0.5,):
+
     if not isinstance(exceptions, tuple):
         raise ValueError('exceptions must be a tuple, not:', type(exceptions))
     while True:
