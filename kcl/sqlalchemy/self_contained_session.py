f1cfd115e4a3c590b83be7dd426edb143ce5fb01
jakeogh <github.com@v6y.net>
2020-12-16 18:56:44 -0700

    auto-commit

--- kcl/sqlalchemy/self_contained_session.py
+++ kcl/sqlalchemy/self_contained_session.py
@@ -6,12 +6,13 @@ import os
 from icecream import ic
 #: (psycopg2.OperationalError) could not connect to server: Connection refused
 from kcl.serviceops import get_latest_postgresql_version
+#from kcl.printops import eprint
+#from sqlalchemy.exc import OperationalError
+from psycopg2 import OperationalError
 from retry_on_exception import retry_on_exception
 from sqlalchemy_utils.functions import create_database, database_exists
 
 from sqlalchemy import create_engine
-#from kcl.printops import eprint
-from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import scoped_session, sessionmaker
 #from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.pool import NullPool
@@ -24,11 +25,11 @@ def start_database(verbose=False):
         ic(command)
     os.system(command)
 
-
 @retry_on_exception(exception=OperationalError,
                     in_e_args="could not connect to server: Connection refused",
                     max_delay=1,
-                    call_function_once=start_database,)
+                    call_function_once=start_database,
+                    verbose=True,)
 def database_already_exists(db_url):
     return database_exists(db_url)
 
