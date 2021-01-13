65b873179c0e72569ee0f505bf3a756da36eda59
jakeogh <github.com@v6y.net>
2021-01-13 14:07:11 -0700

    auto-commit

---
+++ kcl/fdops.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import inspect
+import os
+
+descriptors = set()
+
+
+def update_open_fds():
+    global descriptors
+    fds = set(os.listdir('/proc/self/fd/'))
+    descriptors = fds
+
+
+def print_open_fds(print_all=False):
+    global descriptors
+    (frame, filename, line_number, function_name, lines, index) = inspect.getouterframes(inspect.currentframe())[1]
+    fds = set(os.listdir('/proc/self/fd/'))
+    new_fds = fds - descriptors
+    closed_fds = descriptors - fds
+    descriptors = fds
+
+    if print_all:
+        print("{}:{} ALL file descriptors: {}".format(filename, line_number, fds))
+    if new_fds:
+        print("{}:{} new file descriptors: {}".format(filename, line_number, new_fds))
+    if closed_fds:
+        print("{}:{} closed file descriptors: {}".format(filename, line_number, closed_fds))
+
