609bd0bee763913630f17b79465a17e6b20adbc3
jakeogh <github.com@v6y.net>
2021-01-14 16:49:48 -0700

    auto-commit

--- kcl/memoryops.py
+++ kcl/memoryops.py
@@ -9,7 +9,9 @@ from kcl.commandops import run_command
 
 def pmap():
     command = ['pmap', str(os.getpid())]
-    result = run_command(command)
+    result = run_command(command).decode('utf8')
     result = result.split('\n')
+    result = result[-1]
+    result = result.split(' ')
     result = result[-1]
     return result
